8d9143f1275ab76f6b671dc5fd30cec76abb8cba
Tomas Zemanovic <tzemanovic@gmail.com>
2021-06-22 13:27:36 +0000

    commit 59bd7a06bbc31fc350954decd8275638477ad2ee Merge: 4ff91a7 d19dcea Author: Tomas Zemanovic <tzemanovic@gmail.com> Date:   Tue Jun 22 15:18:08 2021 +0200
        Merge pull request #271 from anomanetwork/fraccaman/gossip
    
        Initial Gossip Implementation

--- rustdoc/implementors/anoma/std/cmp/trait.Ord.js
+++ rustdoc/implementors/anoma/std/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["anoma"] = [];
+implementors["anoma"] = [{"text":"impl <a class=\"trait\" href=\"anoma/std/cmp/trait.Ord.html\" title=\"trait anoma::std::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"anoma/config/struct.PeerAddress.html\" title=\"struct anoma::config::PeerAddress\">PeerAddress</a>","synthetic":false,"types":["anoma::config::PeerAddress"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
