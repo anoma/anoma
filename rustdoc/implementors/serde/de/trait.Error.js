fde89b45df25a69ea45b8efd0f8d56a717372ec6
Fraccaroli Gianmarco <gianmarco.fraccaroli@gmail.com>
2021-06-25 09:43:00 +0000

    commit cb1b7e2238bac697dfc8651a3bbc2dc5beb9dad6 Merge: cc5f68d b8ef8d7 Author: Fraccaroli Gianmarco <gianmarco.fraccaroli@gmail.com> Date:   Fri Jun 25 11:28:58 2021 +0200
        Merge pull request #305 from anomanetwork/fraccaman/ci-fix-push
    
        Add e2e gossip tests

--- rustdoc/implementors/serde/de/trait.Error.js
+++ rustdoc/implementors/serde/de/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["anoma"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.125/serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"anoma/config/enum.SerdeError.html\" title=\"enum anoma::config::SerdeError\">SerdeError</a>","synthetic":false,"types":["anoma::config::SerdeError"]}];
+implementors["anoma"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.126/serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"anoma/config/enum.SerdeError.html\" title=\"enum anoma::config::SerdeError\">SerdeError</a>","synthetic":false,"types":["anoma::config::SerdeError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
