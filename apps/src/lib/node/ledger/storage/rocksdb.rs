ad23b56dffd205e3341527504bf2c86647cc54e8
Tomáš Zemanovič <tomas@heliax.dev>
2021-11-19 15:59:02 +0100

    [apps/node/ledger/rocksdb] increase_parallelism, default to 1/4 CPUs

--- apps/src/lib/node/ledger/storage/rocksdb.rs
+++ apps/src/lib/node/ledger/storage/rocksdb.rs
@@ -17,8 +17,9 @@
 use std::cmp::{min, Ordering};
 use std::collections::HashMap;
 use std::convert::TryInto;
-use std::io;
 use std::path::Path;
+use std::str::FromStr;
+use std::{env, io};
 
 use anoma::ledger::storage::types::PrefixIterator;
 use anoma::ledger::storage::{types, BlockState, DBIter, Error, Result, DB};
@@ -30,13 +31,38 @@ use rocksdb::{
     ReadOptions, SliceTransform, WriteBatch, WriteOptions,
 };
 
+use crate::cli;
+
 // TODO the DB schema will probably need some kind of versioning
+
+/// Env. var to set a number of Rayon global worker threads
+const ENV_VAR_ROCKSDB_COMPACTION_THREADS: &str =
+    "ANOMA_ROCKSDB_COMPACTION_THREADS";
 
 #[derive(Debug)]
 pub struct RocksDB(rocksdb::DB);
 
 /// Open RocksDB for the DB
 pub fn open(path: impl AsRef<Path>) -> Result<RocksDB> {
+    let logical_cores = num_cpus::get() as i32;
+    let compaction_threads =
+        if let Ok(num_str) = env::var(ENV_VAR_ROCKSDB_COMPACTION_THREADS) {
+            match i32::from_str(&num_str) {
+                Ok(num) if num > 0 => num,
+                _ => {
+                    eprintln!(
+                        "Invalid env. var {} value: {}. Expecting a positive \
+                         number.",
+                        ENV_VAR_ROCKSDB_COMPACTION_THREADS, num_str
+                    );
+                    cli::safe_exit(1)
+                }
+            }
+        } else {
+            // If not set, default to quarter of logical CPUs count
+            logical_cores / 4
+        };
+
     let max_open_files = match increase_nofile_limit() {
         Ok(max_open_files) => Some(max_open_files),
         Err(err) => {
@@ -47,8 +73,11 @@ pub fn open(path: impl AsRef<Path>) -> Result<RocksDB> {
     let mut cf_opts = Options::default();
     // ! recommended initial setup https://github.com/facebook/rocksdb/wiki/Setup-Options-and-Basic-Tuning#other-general-options
     cf_opts.set_level_compaction_dynamic_level_bytes(true);
-    // compactions + flushes
-    cf_opts.set_max_background_jobs(6);
+
+    // This gives `compaction_threads` number to compaction threads and 1 thread
+    // for flush background jobs: https://github.com/facebook/rocksdb/blob/17ce1ca48be53ba29138f92dafc9c853d9241377/options/options.cc#L622
+    cf_opts.increase_parallelism(compaction_threads);
+
     cf_opts.set_bytes_per_sync(1048576);
     if let Some(max_open_files) =
         max_open_files.and_then(|max| max.as_raw().try_into().ok())
