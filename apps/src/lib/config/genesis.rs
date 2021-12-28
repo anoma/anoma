024c62ca72255d477d30769994e660e5c4f9e1e5
Jacob Turner <jacob.turner870@gmail.com>
2021-12-28 09:50:35 +0100

    Update apps/src/lib/config/genesis.rs
    Co-authored-by: Tomas Zemanovic <tomas@heliax.dev>

--- apps/src/lib/config/genesis.rs
+++ apps/src/lib/config/genesis.rs
@@ -622,7 +622,6 @@ pub fn genesis() -> Genesis {
         },
         account_key: account_keypair.public,
         non_staked_balance: token::Amount::whole(100_000),
-        // TODO replace with https://github.com/anoma/anoma/issues/25)
         validator_vp_code_path: vp_validator_path.into(),
         validator_vp_sha256: Default::default(),
         reward_vp_code_path: vp_user_path.into(),
