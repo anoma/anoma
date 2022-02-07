6672d49849182c39cd35f55388f0ab33ffc257cb
Tomáš Zemanovič <tomas@heliax.dev>
2022-02-07 16:21:42 +0100

    Revert "wip: try to feature-gate imports in WASM host env"
    This reverts commit dcff73563a6147f3ab74ac93f4ee04ea27ff0a5d.

--- vm_env/src/lib.rs
+++ vm_env/src/lib.rs
@@ -6,53 +6,41 @@
 #![deny(rustdoc::broken_intra_doc_links)]
 #![deny(rustdoc::private_intra_doc_links)]
 
-#[cfg(all(feature = "ibc", feature = "import-defs"))]
+#[cfg(feature = "ibc")]
 pub mod ibc;
-#[cfg(feature = "import-defs")]
 pub mod imports;
-#[cfg(feature = "import-defs")]
 pub mod intent;
-#[cfg(feature = "import-defs")]
 pub mod key;
-#[cfg(feature = "import-defs")]
 pub mod proof_of_stake;
-#[cfg(feature = "import-defs")]
 pub mod token;
 
-#[cfg(feature = "import-defs")]
-mod vm_env {
+pub mod tx_prelude {
+    pub use anoma::types::address::Address;
+    pub use anoma::types::*;
+    pub use anoma_macros::transaction;
 
-    pub mod tx_prelude {
-        pub use anoma::types::address::Address;
-        pub use anoma::types::*;
-        pub use anoma_macros::transaction;
-
-        #[cfg(feature = "ibc")]
-        pub use crate::ibc::{Ibc, IbcActions};
-        pub use crate::imports::tx::*;
-        pub use crate::intent::tx as intent;
-        pub use crate::proof_of_stake::{self, PoS, PosRead, PosWrite};
-        pub use crate::token::tx as token;
-    }
-
-    pub mod vp_prelude {
-        // used in the VP input
-        pub use std::collections::HashSet;
-
-        pub use anoma::types::address::Address;
-        pub use anoma::types::*;
-        pub use anoma_macros::validity_predicate;
-
-        pub use crate::imports::vp::*;
-        pub use crate::intent::vp as intent;
-        pub mod key {
-            pub use crate::key::ed25519::vp as ed25519;
-        }
-        pub use anoma::ledger::pos as proof_of_stake;
-
-        pub use crate::token::vp as token;
-    }
+    #[cfg(feature = "ibc")]
+    pub use crate::ibc::{Ibc, IbcActions};
+    pub use crate::imports::tx::*;
+    pub use crate::intent::tx as intent;
+    pub use crate::proof_of_stake::{self, PoS, PosRead, PosWrite};
+    pub use crate::token::tx as token;
 }
 
-#[cfg(feature = "import-defs")]
-pub use vm_env::*;
+pub mod vp_prelude {
+    // used in the VP input
+    pub use std::collections::HashSet;
+
+    pub use anoma::types::address::Address;
+    pub use anoma::types::*;
+    pub use anoma_macros::validity_predicate;
+
+    pub use crate::imports::vp::*;
+    pub use crate::intent::vp as intent;
+    pub mod key {
+        pub use crate::key::ed25519::vp as ed25519;
+    }
+    pub use anoma::ledger::pos as proof_of_stake;
+
+    pub use crate::token::vp as token;
+}
