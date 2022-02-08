6becfd085384d79fa9a8e8ede4dd64a4a7b6b36b
Tomáš Zemanovič <tomas@heliax.dev>
2022-02-08 15:08:17 +0100

    Revert "wip: try to exclude vm_env modules from tarpaulin"
    This reverts commit 6264be3bfe7f72bafe604accc40699ca1a98fae1.

--- vm_env/src/lib.rs
+++ vm_env/src/lib.rs
@@ -1,62 +1,46 @@
 //! This crate contains library code for wasm. Some of the code is re-exported
 //! from the `shared` crate.
-//!
-//! Note that all the modules in here depend on the `imports` module, which
-//! currently cannot be compiled by tarpaulin (test coverage tool), hence all
-//! the modules in here are not included in the tarpaulin report
-//! (`#[cfg(not(tarpaulin_include))]`).
 
 #![doc(html_favicon_url = "https://docs.anoma.network/favicon.png")]
 #![doc(html_logo_url = "https://docs.anoma.network/rustdoc-logo.png")]
 #![deny(rustdoc::broken_intra_doc_links)]
 #![deny(rustdoc::private_intra_doc_links)]
 
-#[cfg(all(feature = "ibc", not(tarpaulin_include)))]
+#[cfg(feature = "ibc")]
 pub mod ibc;
-#[cfg(not(tarpaulin_include))]
 pub mod imports;
-#[cfg(not(tarpaulin_include))]
 pub mod intent;
-#[cfg(not(tarpaulin_include))]
 pub mod key;
-#[cfg(not(tarpaulin_include))]
 pub mod proof_of_stake;
-#[cfg(not(tarpaulin_include))]
 pub mod token;
 
-#[cfg(not(tarpaulin_include))]
-mod vm_env {
+pub mod tx_prelude {
+    pub use anoma::types::address::Address;
+    pub use anoma::types::*;
+    pub use anoma_macros::transaction;
 
-    pub mod tx_prelude {
-        pub use anoma::types::address::Address;
-        pub use anoma::types::*;
-        pub use anoma_macros::transaction;
-
-        #[cfg(feature = "ibc")]
-        pub use crate::ibc::{Ibc, IbcActions};
-        pub use crate::imports::tx::*;
-        pub use crate::intent::tx as intent;
-        pub use crate::proof_of_stake::{self, PoS, PosRead, PosWrite};
-        pub use crate::token::tx as token;
-    }
-
-    pub mod vp_prelude {
-        // used in the VP input
-        pub use std::collections::HashSet;
-
-        pub use anoma::types::address::Address;
-        pub use anoma::types::*;
-        pub use anoma_macros::validity_predicate;
-
-        pub use crate::imports::vp::*;
-        pub use crate::intent::vp as intent;
-        pub mod key {
-            pub use crate::key::ed25519::vp as ed25519;
-        }
-        pub use anoma::ledger::pos as proof_of_stake;
-
-        pub use crate::token::vp as token;
-    }
+    #[cfg(feature = "ibc")]
+    pub use crate::ibc::{Ibc, IbcActions};
+    pub use crate::imports::tx::*;
+    pub use crate::intent::tx as intent;
+    pub use crate::proof_of_stake::{self, PoS, PosRead, PosWrite};
+    pub use crate::token::tx as token;
 }
 
-pub use vm_env::*;
+pub mod vp_prelude {
+    // used in the VP input
+    pub use std::collections::HashSet;
+
+    pub use anoma::types::address::Address;
+    pub use anoma::types::*;
+    pub use anoma_macros::validity_predicate;
+
+    pub use crate::imports::vp::*;
+    pub use crate::intent::vp as intent;
+    pub mod key {
+        pub use crate::key::ed25519::vp as ed25519;
+    }
+    pub use anoma::ledger::pos as proof_of_stake;
+
+    pub use crate::token::vp as token;
+}
