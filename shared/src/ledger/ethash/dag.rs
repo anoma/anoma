9df3fa9c293009c0dd7fb05f53847b1bf09ea221
R2D2 <jacob.turner870@gmail.com>
2022-03-28 23:07:51 +0200

    [chore]: Added unit tests for verifyig eth headers

--- shared/src/ledger/ethash/dag.rs
+++ shared/src/ledger/ethash/dag.rs
@@ -3,6 +3,7 @@
 pub mod dag_cache {
     use std::thread::JoinHandle;
 
+    use borsh::{BorshDeserialize, BorshSerialize};
     use ethereum_types::{H256, H64, U256};
     use tokio::sync::oneshot::{channel, Receiver};
 
@@ -52,7 +53,7 @@ pub mod dag_cache {
     }
 
     /// A pseudorandom cache used to verify Ethereum headers
-    #[derive(Default)]
+    #[derive(Default, BorshSerialize, BorshDeserialize)]
     pub struct Cache {
         /// size of the cache
         pub full_size: usize,
@@ -66,6 +67,7 @@ pub mod dag_cache {
     ///
     /// We thus pipeline the caches and pre-compute upcoming
     /// caches in the background
+    #[derive(BorshSerialize, BorshDeserialize)]
     pub struct LightDAG {
         /// The current epoch
         epoch: usize,
@@ -79,6 +81,7 @@ pub mod dag_cache {
         pub update_ahead: u64,
         /// Cache for the next epoch
         pub next_cache: Option<Cache>,
+        #[borsh_skip]
         /// Background task to update the cache asynchronously
         update: Option<BackgroundTask<Cache>>,
     }
@@ -118,11 +121,6 @@ pub mod dag_cache {
         ///  2. Keeps the cache for the previous epoch for `self.keep_previous`
         ///     blocks afterwards. Then it is dropped.
         pub fn update(&mut self, number: u64) {
-            // update the epoch
-            self.epoch = std::cmp::max(
-                (number / EPOCH_LENGTH as u64) as usize,
-                self.epoch,
-            );
             // this runs in the background asynchronously
             self.begin_precompute(number);
             // check if background task finished and store result
@@ -137,10 +135,11 @@ pub mod dag_cache {
         /// epoch, begin pre-computing the next cache in the background
         fn begin_precompute(&mut self, number: u64) {
             if self.update.is_none()
+                && self.next_cache.is_none()
                 && !self.is_valid_for(number + self.update_ahead)
             {
                 let epoch =
-                    (number + self.update_ahead / EPOCH_LENGTH as u64) as usize;
+                    ((number + self.update_ahead) / EPOCH_LENGTH as u64) as usize;
                 self.update = Some(BackgroundTask::new(move || {
                     let cache_size = get_cache_size(epoch);
                     let mut cache: Vec<u8> = vec![0; cache_size];
@@ -161,6 +160,7 @@ pub mod dag_cache {
                 self.update.as_mut().map(|task| task.check_finished())
             {
                 self.next_cache = Some(new_cache);
+                self.update = None;
             }
         }
 
@@ -175,10 +175,13 @@ pub mod dag_cache {
                     self.previous_cache.as_mut().unwrap(),
                     &mut self.cache,
                 );
-                self.cache = self.next_cache.take().expect(
-                    "Cache for the next epoch should have been pre-computed",
-                );
+                self.cache = self.next_cache.take().unwrap_or_default();
             }
+            // update the epoch
+            self.epoch = std::cmp::max(
+                (number / EPOCH_LENGTH as u64) as usize,
+                self.epoch,
+            );
         }
 
         /// If the current block number is at least `self.keep_previous` blocks
@@ -256,13 +259,20 @@ pub mod dag_cache {
 
     #[cfg(test)]
     mod tests {
+        use borsh::BorshDeserialize;
         use ethereum_types::{H256, H64, U256};
         use hex_literal::*;
 
-        use super::{cross_boundary, LightDAG};
+        use super::LightDAG;
+        use crate::types::ethereum_headers::{Difficulty, EthereumHeader};
+        use crate::types::hash::Hash;
 
+        const DAG_PATH: &str = "/home/r2d2/Projects/anoma/shared/src/ledger/ethash/test_data/eth_pseudo_cache";
+
+        /// Test that verifying an Ethereum header works, even if it is
+        /// for an epoch whose cache is not in memory.
         #[test]
-        fn hashimoto_should_work() {
+        fn hashimoto_no_precomputed_cache() {
             let light_dag = LightDAG {
                 epoch: 0,
                 previous_cache: None,
@@ -272,27 +282,208 @@ pub mod dag_cache {
                 next_cache: None,
                 update: None,
             };
-            // bare_hash of block#8996777 on ethereum mainnet
-            let partial_header_hash = H256::from(hex!(
-            "3c2e6623b1de8862a927eeeef2b6b25dea6e1d9dad88dca3c239be3959dc384a"
-        ));
+            let header = EthereumHeader {
+                // bare_hash of block#8996777 on ethereum mainnet
+                hash: H256::from(hex!(
+                    "3c2e6623b1de8862a927eeeef2b6b25dea6e1d9dad88dca3c239be3959dc384a"
+                )).into(),
+                parent_hash: Hash([0; 32]),
+                number: 8996777,
+                difficulty: Difficulty::from(U256::from(2580289863567664_u128)),
+                nonce: H64::from(hex!("a5d3d0ccc8bb8a29")).into(),
+                mix_hash: H256::from(hex!(
+                    "543bc0769f7d5df30e7633f4a01552c2cee7baace8a6da37fddaa19e49e81209"
+                )).into(),
+                state_root: Hash([0; 32]),
+                transactions_root: Hash([0; 32]),
+            };
+            // this should work even though the cache is not precomputed
+            assert!(light_dag.verify_header(header));
+        }
 
-            let (mixh, final_hash) = light_dag.compute_slow(
-                partial_header_hash,
-                H64::from(hex!("a5d3d0ccc8bb8a29")),
-                8996777,
-            );
-            assert_eq!(
-                mixh,
-                H256::from(hex!(
-                "543bc0769f7d5df30e7633f4a01552c2cee7baace8a6da37fddaa19e49e81209"
-                ))
-            );
+        /// Test that verifying an Ethereum header in the same
+        /// epoch as the LightDAG is fast.
+        #[test]
+        fn test_ethereum_in_current_epoch() {
+            // LightDAG::new(8996777, 0, 0);
+            let mut light_dag: LightDAG = BorshDeserialize::try_from_slice(
+                std::fs::read(DAG_PATH)
+                    .expect("Test failed")
+                    .as_slice()
+            )
+            .expect("Test failed");
+            let header = EthereumHeader {
+                // bare_hash of block#8996777 on ethereum mainnet
+                hash: H256::from(hex!(
+                    "3c2e6623b1de8862a927eeeef2b6b25dea6e1d9dad88dca3c239be3959dc384a"
+                )).into(),
+                parent_hash: Hash([0; 32]),
+                number: 8996777,
+                difficulty: Difficulty::from(U256::from(2580289863567664_u128)),
+                nonce: H64::from(hex!("a5d3d0ccc8bb8a29")).into(),
+                mix_hash: H256::from(hex!(
+                    "543bc0769f7d5df30e7633f4a01552c2cee7baace8a6da37fddaa19e49e81209"
+                )).into(),
+                state_root: Hash([0; 32]),
+                transactions_root: Hash([0; 32]),
+            };
+            // This should ensure the current cache is valid for this header.
+            assert!(light_dag.is_valid_for(header.number));
+            // Test that verification passes and is fast enough
+            let start = std::time::Instant::now();
+            assert!(light_dag.verify_header(header));
+            let time_taken = std::time::Instant::now() - start;
+            assert!(time_taken.as_secs() <= 5);
 
-            assert!(
-                U256::from(final_hash.0)
-                    <= cross_boundary(2580289863567664_u128.into())
-            );
+            // We now test that update is a no-op here.
+            let old_epoch = light_dag.epoch.clone();
+            light_dag.update(8996777);
+            assert_eq!(light_dag.epoch, old_epoch);
+            assert!(light_dag.update.is_none());
+            assert!(light_dag.previous_cache.is_none());
+            assert!(light_dag.next_cache.is_none());
+            assert!(light_dag.update.is_none());
+        }
+
+        /// Test that update advances to the next epoch if it sees a header in the next
+        /// epoch. The current epoch should move to the previous epoch and can still
+        /// be used to verify headers.
+        #[test]
+        fn test_ethereum_in_previous_epoch() {
+            // LightDAG::new(8996777, 0, 0);
+            let mut light_dag: LightDAG = BorshDeserialize::try_from_slice(
+                std::fs::read(DAG_PATH)
+                    .expect("Test failed")
+                    .as_slice()
+            )
+            .expect("Test failed");
+            light_dag.keep_previous = 4000;
+
+            let header = EthereumHeader {
+                // bare_hash of block#8996777 on ethereum mainnet
+                hash: H256::from(hex!(
+                    "3c2e6623b1de8862a927eeeef2b6b25dea6e1d9dad88dca3c239be3959dc384a"
+                )).into(),
+                parent_hash: Hash([0; 32]),
+                number: 8996777,
+                difficulty: Difficulty::from(U256::from(2580289863567664_u128)),
+                nonce: H64::from(hex!("a5d3d0ccc8bb8a29")).into(),
+                mix_hash: H256::from(hex!(
+                    "543bc0769f7d5df30e7633f4a01552c2cee7baace8a6da37fddaa19e49e81209"
+                )).into(),
+                state_root: Hash([0; 32]),
+                transactions_root: Hash([0; 32]),
+            };
+            // check that the epoch changed and pipeline advanced correctly
+            // pre-update
+            assert!(light_dag.previous_cache.is_none());
+            assert!(!light_dag.cache.cache.is_empty());
+            assert!(light_dag.next_cache.is_none());
+            assert!(light_dag.update.is_none());
+            let old_epoch = light_dag.epoch.clone();
+
+            light_dag.update(9000000);
+            // post-update
+            assert_eq!(old_epoch + 1, light_dag.epoch);
+            assert!(light_dag.previous_cache.is_some());
+            assert!(light_dag.cache.cache.is_empty());
+            assert!(light_dag.next_cache.is_none());
+            assert!(light_dag.update.is_some());
+
+            // Test that verification passes and is fast
+            let start = std::time::Instant::now();
+            assert!(light_dag.verify_header(header));
+            let time_taken = std::time::Instant::now() - start;
+            assert!(time_taken.as_secs() <= 5);
+
+            // this does not change the epoch. So it should be a no-op
+            light_dag.update(9000001);
+            // post-update
+            assert_eq!(old_epoch + 1, light_dag.epoch);
+            assert!(light_dag.previous_cache.is_some());
+            assert!(light_dag.cache.cache.is_empty());
+            assert!(light_dag.next_cache.is_none());
+            assert!(light_dag.update.is_some());
+        }
+
+        /// Test that old caches are dropped once they are `keep_previous` or
+        /// more blocks behind the start of the current epoch
+        #[test]
+        fn test_drop_old_cache() {
+            // LightDAG::new(8996777, 0, 0);
+            let mut light_dag: LightDAG = BorshDeserialize::try_from_slice(
+                std::fs::read(DAG_PATH)
+                    .expect("Test failed")
+                    .as_slice()
+            )
+            .expect("Test failed");
+            light_dag.keep_previous = 1;
+            // check that the epoch changed and pipeline advanced correctly
+            // pre-update
+            assert!(light_dag.previous_cache.is_none());
+            assert!(!light_dag.cache.cache.is_empty());
+            assert!(light_dag.next_cache.is_none());
+            assert!(light_dag.update.is_none());
+            let old_epoch = light_dag.epoch.clone();
+
+            light_dag.update(9000000);
+            // post-update
+            assert_eq!(old_epoch + 1, light_dag.epoch);
+            assert!(light_dag.previous_cache.is_some());
+            assert!(light_dag.cache.cache.is_empty());
+            assert!(light_dag.next_cache.is_none());
+            assert!(light_dag.update.is_some());
+
+            light_dag.update(9000001);
+            // post-update
+            assert_eq!(old_epoch + 1, light_dag.epoch);
+            assert!(light_dag.previous_cache.is_none());
+            assert!(light_dag.cache.cache.is_empty());
+            assert!(light_dag.next_cache.is_none());
+            assert!(light_dag.update.is_some());
+        }
+
+        /// Test that at `update_ahead` blocks prior to a new epoch, a background
+        /// process begins computing the next cache. This should eventually be
+        /// stored in the `next_cache` field
+        #[test]
+        fn test_background_process() {
+            // LightDAG::new(8996777, 0, 0);
+            let mut light_dag: LightDAG = BorshDeserialize::try_from_slice(
+                std::fs::read(DAG_PATH)
+                    .expect("Test failed")
+                    .as_slice()
+            )
+            .expect("Test failed");
+            // set the pipeline length to something realistic
+            light_dag.update_ahead = 200;
+            // we start updating `update_ahead` blocks ahead.
+            let mut block_number = 9000000 - 200;
+            light_dag.update(block_number);
+            // this makes it easier to check that the current cache gets updated
+            light_dag.cache.cache = vec![];
+            // Background process should have started
+            assert!(light_dag.update.is_some());
+            let start = std::time::Instant::now();
+            // It should not take longer than 200 seconds for this to finish
+            while (std::time::Instant::now() - start).as_secs() < 300 {
+                std::thread::sleep(std::time::Duration::from_secs(2));
+                block_number += 1;
+                // we should be able to keep updating while this happens
+                light_dag.update(block_number);
+                // the process has finished.
+                if light_dag.update.is_none() {
+                    break;
+                }
+            }
+            assert!(block_number < (9000000 - 1));
+            // we should now have the result stored in `next_cache`
+            assert!(light_dag.next_cache.is_some());
+            light_dag.update(9000000 - 1);
+            // check that the new cache is stored
+            assert!(!light_dag.cache.cache.is_empty());
+            // check that there is no longer a background process.
+            assert!(light_dag.update.is_none());
         }
     }
 }
