20bfb08855af2396e050776321506fa14c783262
James Hiew <james@chainfork.com>
2022-04-22 17:41:12 +0100

    WIP - crude queue based on Vec<TransferFromEthereum>

--- shared/src/types/transaction/eth_bridge.rs
+++ shared/src/types/transaction/eth_bridge.rs
@@ -1,8 +1,27 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 
+use crate::types::address::Address;
+use crate::types::token::Amount;
+
+// TODO: custom type for queue - Vec<TransferFromEthereum>
+
+#[derive(Debug, Clone, PartialEq, BorshSerialize, BorshDeserialize)]
+pub struct TransferFromEthereum {
+    /// the address on Namada receiving the tokens
+    pub receiver: Address,
+    /// the amount of ETH token to mint
+    pub amount: Amount,
+    /// minimum number of confirmations needed for mints
+    pub min_confirmations: u8,
+    /// height of the block at which the message appeared
+    pub height: u64,
+    // the hash & height of the last descendant block marked as `seen`
+    // latest_descendant: ([u8; 32], u64)  // TODO
+}
+
 /// Contains information to update the queue
 #[derive(Debug, Clone, PartialEq, BorshSerialize, BorshDeserialize)]
 pub struct UpdateQueue {
-    /// The newval to set queue to
-    pub newval: u64,
+    /// New transfers to add to the queue
+    pub enqueue: Vec<TransferFromEthereum>,
 }
--- vm_env/src/eth_bridge.rs
+++ vm_env/src/eth_bridge.rs
@@ -1,13 +1,36 @@
 /// Tx imports and functions.
 pub mod tx {
     use anoma::ledger::eth_bridge::storage;
-    use anoma::types::transaction::eth_bridge::UpdateQueue;
+    use anoma::ledger::eth_bridge::storage::queue_key;
+    use anoma::types::transaction::eth_bridge::{
+        TransferFromEthereum, UpdateQueue,
+    };
 
     use crate::imports::tx;
+    use crate::tx_prelude::log_string;
 
     pub fn update_queue(data: UpdateQueue) {
+        log_string(format!(
+            "update_queue tx being executed ({} messages to enqueue)",
+            data.enqueue.len()
+        ));
         let queue_key = storage::queue_key();
-        let newval = data.newval;
-        tx::write(queue_key.to_string(), newval);
+        let mut queue: Vec<TransferFromEthereum> =
+            if tx::has_key(queue_key.to_string()) {
+                log_string("queue key exists");
+                tx::read(queue_key.to_string()).unwrap()
+            } else {
+                log_string("initializing queue for the first time");
+                tx::write(
+                    queue_key.to_string(),
+                    Vec::<TransferFromEthereum>::new(),
+                );
+                vec![]
+            };
+        log_string(format!("got existing queue: {:#?}", queue));
+        for transfer in data.enqueue {
+            queue.push(transfer);
+        }
+        tx::write(queue_key.to_string(), queue);
     }
 }
