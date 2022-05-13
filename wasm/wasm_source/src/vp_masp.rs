e0b03b3afbca15bf5cce0770e5021b7376fda86e
Murisi Tarusenga <murisit@gmail.com>
2022-05-13 13:48:33 +0200

    Require that new assets added to the MASP have the correct timestamp.

--- wasm/wasm_source/src/vp_masp.rs
+++ wasm/wasm_source/src/vp_masp.rs
@@ -2,6 +2,26 @@
 #[cfg(feature = "vp_masp")]
 pub mod vp_masp {
     use anoma_vp_prelude::*;
+    use masp_primitives::asset_type::AssetType;
+    use masp_primitives::transaction::components::Amount;
+    use anoma_vp_prelude::address::masp;
+
+    /// Convert Anoma amount and token type to MASP equivalents
+    fn convert_amount(
+        token: &Address,
+        val: token::Amount,
+    ) -> (AssetType, Amount) {
+        let epoch = get_block_epoch();
+        // Timestamp the chosen token with the current epoch
+        let token_bytes = (token, epoch.0).try_to_vec().expect("token should serialize");
+        // Generate the unique asset identifier from the unique token address
+        let asset_type = AssetType::new(token_bytes.as_ref())
+            .expect("unable to create asset type");
+        // Combine the value and unit into one amount
+        let amount = Amount::from_nonnegative(asset_type, u64::from(val))
+            .expect("invalid value or asset type for amount");
+        (asset_type, amount)
+    }
 
     #[validity_predicate]
     fn validate_tx(
@@ -19,9 +39,36 @@ pub mod vp_masp {
 
         let signed =
             SignedTxData::try_from_slice(&tx_data[..]).unwrap();
-        let _transfer =
+        let transfer =
             token::Transfer::try_from_slice(&signed.data.unwrap()[..]).unwrap();
 
+        if let Some(shielded_tx) = transfer.shielded {
+            let mut transparent_tx_pool = Amount::zero();
+            // The Sapling value balance adds to the transparent tx pool
+            transparent_tx_pool += shielded_tx.value_balance.clone();
+            // Note that the asset type is timestamped so shields/unshields where
+            // the shielded value has an incorrect timestamp are automatically
+            // rejected
+            let (_transp_asset, transp_amt) = convert_amount(&transfer.token, transfer.amount);
+            if transfer.source != masp() {
+                // Non-masp sources add to transparent tx pool
+                transparent_tx_pool += transp_amt.clone();
+            }
+            if transfer.target != masp() {
+                // Non-masp destinations subtract from transparent tx pool
+                transparent_tx_pool -= transp_amt.clone();
+            }
+            
+            if !(transparent_tx_pool >= Amount::zero()) {
+                debug_log!("Transparent transaction value pool must be nonnegative. \
+                            Violation may be caused by transaction being constructed \
+                            in previous epoch. Maybe try again.");
+                // Section 3.4: The remaining value in the transparent
+                // transaction value pool MUST be nonnegative.
+                return false;
+            }
+        }
+        
         return true;
     }
 }
