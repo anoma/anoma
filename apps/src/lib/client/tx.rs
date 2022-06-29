6954022b74b1b02492943e0376533c71d3b0ec52
brentstone <brentstone01@gmail.com>
2022-06-29 22:07:21 +0200

    allow CL specification of a specific key scheme for the TxInitValidator

--- apps/src/lib/client/tx.rs
+++ apps/src/lib/client/tx.rs
@@ -180,6 +180,7 @@ pub async fn submit_init_validator(
     args::TxInitValidator {
         tx: tx_args,
         source,
+        scheme,
         account_key,
         consensus_key,
         rewards_account_key,
@@ -202,7 +203,7 @@ pub async fn submit_init_validator(
         println!("Generating validator account key...");
         ctx.wallet
             .gen_key(
-                SchemeType::Ed25519,
+                scheme,
                 Some(validator_key_alias.clone()),
                 unsafe_dont_encrypt
             )
@@ -215,7 +216,7 @@ pub async fn submit_init_validator(
             println!("Generating consensus key...");
             ctx.wallet
                 .gen_key(
-                    SchemeType::Ed25519,
+                    scheme,
                     Some(consensus_key_alias.clone()),
                     unsafe_dont_encrypt
                 )
@@ -227,7 +228,7 @@ pub async fn submit_init_validator(
             println!("Generating staking reward account key...");
             ctx.wallet
                 .gen_key(
-                    SchemeType::Ed25519,
+                    scheme,
                     Some(rewards_key_alias.clone()),
                     unsafe_dont_encrypt
                 )
