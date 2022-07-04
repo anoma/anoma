ee269e045a076fc8ba39a496dbe9ff8fa0ece05a
brentstone <brentstone01@gmail.com>
2022-07-04 14:00:30 +0200

    e2e test_genesis_validators(): make each validator have different key scheme

--- tests/src/e2e/ledger_tests.rs
+++ tests/src/e2e/ledger_tests.rs
@@ -1599,7 +1599,7 @@ fn test_genesis_validators() -> Result<()> {
             }
     };
 
-    // 1. Setup 2 genesis validators
+    // 1. Setup 2 genesis validators, one with ed25519 keys (0) and one with secp256k1 keys (1)
     let validator_0_alias = "validator-0";
     let validator_1_alias = "validator-1";
 
@@ -1611,6 +1611,7 @@ fn test_genesis_validators() -> Result<()> {
             "--unsafe-dont-encrypt",
             "--alias",
             validator_0_alias,
+            "--scheme ed25519",
             "--net-address",
             &format!("127.0.0.1:{}", get_first_port(0)),
         ],
@@ -1647,6 +1648,7 @@ fn test_genesis_validators() -> Result<()> {
             "--unsafe-dont-encrypt",
             "--alias",
             validator_1_alias,
+            "--scheme secp256k1",
             "--net-address",
             &format!("127.0.0.1:{}", get_first_port(1)),
         ],
