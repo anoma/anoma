a682e77c2a5461495206f3c5d6f7f587ad06e7af
Murisi Tarusenga <murisit@gmail.com>
2022-07-12 06:20:24 +0200

    Corrected native function for get_tx_index in tests..

--- tests/src/vm_host_env/tx.rs
+++ tests/src/vm_host_env/tx.rs
@@ -348,6 +348,7 @@ mod native_tx_host_env {
     native_host_fn!(tx_emit_ibc_event(event_ptr: u64, event_len: u64));
     native_host_fn!(tx_get_chain_id(result_ptr: u64));
     native_host_fn!(tx_get_block_height() -> u64);
+    native_host_fn!(tx_get_tx_index() -> u32);
     native_host_fn!(tx_get_block_time() -> i64);
     native_host_fn!(tx_get_block_hash(result_ptr: u64));
     native_host_fn!(tx_get_block_epoch() -> u64);
