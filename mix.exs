4d366a7f755ff4d12ee8581c96a71f1b99218f68
mariari <mariari@protonmail.com>
2023-07-18 00:21:44 +0800

    Add in deps which are useful for development
    - typed_struct to get a useful type declaration syntax
    - dialyxir to get dialyzer be standalone instead of just in the IDE

--- mix.exs
+++ mix.exs
@@ -21,6 +21,8 @@ defmodule Anoma.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:typed_struct, "~> 0.3.0"},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
