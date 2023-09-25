f5d5d0fa998d201a0b273cce9e69da4c4386d916
mariari <mariari@protonmail.com>
2023-09-26 05:14:40 +0800

    Add Rustler as a dependnecy
    We add Ruslter 29.1 to the dependncy list, this also brings in the
    following dependencies:
    
    1. jason
    2. toml

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Anoma.MixProject do
   defp deps do
     [
       {:typed_struct, "~> 0.3.0"},
-      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
+      {:rustler, "~> 0.29.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
