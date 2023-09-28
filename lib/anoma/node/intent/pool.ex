82bb9cc27c1a454453d160e2e41dfe11160198bc
mariari <mariari@protonmail.com>
2023-09-29 02:56:20 +0800

    Add Basic Intent Pool functionality
    We add the functionality of:
    1. adding intents
    2. removing intents
    3. requesting all intents

--- lib/anoma/node/intent/pool.ex
+++ lib/anoma/node/intent/pool.ex
@@ -10,8 +10,9 @@ defmodule Anoma.Node.Intent.Pool do
   alias Anoma.Intent
   alias Anoma.Node.Utility
 
+  @type intents :: MapSet.t(Intent.t())
   typedstruct do
-    field(:intents, MapSet.t(Intent.t()), default: MapSet.new())
+    field(:intents, intents, default: MapSet.new())
   end
 
   def init(_init) do
@@ -20,5 +21,56 @@ defmodule Anoma.Node.Intent.Pool do
 
   def start_link(arg) do
     GenServer.start_link(__MODULE__, arg, Utility.name(arg))
+  end
+
+  ############################################################
+  #                      Public RPC API                      #
+  ############################################################
+
+  @spec intents(GenServer.server()) :: intents
+  def intents(pool) do
+    GenServer.call(pool, :intents)
+  end
+
+  @spec new_intent(GenServer.server(), Intent.t()) :: :ok
+  def new_intent(pool, intent) do
+    GenServer.cast(pool, {:new_intent, intent})
+  end
+
+  @spec remove_intent(GenServer.server(), Intent.t()) :: :ok
+  def remove_intent(pool, intent) do
+    GenServer.cast(pool, {:remove_intent, intent})
+  end
+
+  ############################################################
+  #                    Genserver Behavior                    #
+  ############################################################
+
+  def handle_cast({:new_intent, intent}, pool) do
+    new_pool = handle_new_intent(pool, intent)
+    {:noreply, new_pool}
+  end
+
+  def handle_cast({:remove_intent, intent}, pool) do
+    new_pool = handle_remove_intent(pool, intent)
+    {:noreply, new_pool}
+  end
+
+  def handle_call(:intents, _from, pool) do
+    {:reply, pool.intents, pool}
+  end
+
+  ############################################################
+  #                  Genserver Implementation                #
+  ############################################################
+
+  @spec handle_remove_intent(t(), Intent.t()) :: t()
+  defp handle_remove_intent(pool, intent) do
+    %Pool{pool | intents: MapSet.delete(pool.intents, intent)}
+  end
+
+  @spec handle_new_intent(t(), Intent.t()) :: t()
+  defp handle_new_intent(pool, intent) do
+    %Pool{pool | intents: MapSet.put(pool.intents, intent)}
   end
 end
