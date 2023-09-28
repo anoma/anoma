79c971d1906b4faa4c048df6c3e022748ab66d73
mariari <mariari@protonmail.com>
2023-09-29 03:39:43 +0800

    Add Testing for the Intent Pool
    The intent pool now has proper testing showing how it works:
    
    Oddly enough the shutdown calls are not needed, as I think the exunit
    automatically does it for you

---
+++ test/intent_test.exs
@@ -0,0 +1,53 @@
+defmodule AnomaTest.Intent do
+  use ExUnit.Case, async: true
+
+  alias Anoma.Node.Intent
+  alias Anoma.Node.Intent.{Communicator, Pool}
+  alias Anoma.Resource
+
+  doctest(Anoma.Node.Intent)
+
+  test "Intents don't double add" do
+    {:ok, supervisor} = Intent.start_link(:intents_add)
+
+    resource = %Resource{quantity: 5}
+    resource_set = MapSet.new([resource])
+
+    Communicator.new_intent(:intents_add_com, resource)
+    Communicator.subscribe(:intents_add_com, self())
+    assert_receive {:"$gen_cast", {:intents, resource_set}}
+
+    Communicator.new_intent(:intents_add_com, resource)
+
+    assert Communicator.all_intents(:intents_add_com) == resource_set
+
+    # Odd this isn't needed?
+    Intent.shutdown(supervisor)
+  end
+
+  test "intents remove properly" do
+    {:ok, supervisor} = Intent.start_link(:intents_rem)
+
+    resource = %Resource{quantity: 5}
+    Communicator.new_intent(:intents_rem_com, resource)
+    Communicator.remove_intent(:intents_rem_com, resource)
+
+    assert Communicator.all_intents(:intents_rem_com) == MapSet.new()
+
+    Intent.shutdown(supervisor)
+  end
+
+  test "Intents signal on any additions" do
+    {:ok, supervisor} = Intent.start_link(:intents_signal)
+    resource_1 = %Resource{quantity: 6}
+    Communicator.subscribe(:intents_signal_com, self())
+
+    Communicator.new_intent(:intents_signal_com, resource_1)
+    Communicator.new_intent(:intents_signal_com, resource_1)
+
+    assert_receive {:"$gen_cast", {:new_intent, resource_1}}
+    assert_receive {:"$gen_cast", {:new_intent, resource_1}}
+
+    Intent.shutdown(supervisor)
+  end
+end
