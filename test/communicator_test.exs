c3cc9ba2e1802c3141d1c44134c0b84cc70fceff
mariari <mariari@protonmail.com>
2023-10-05 06:58:08 +0800

    Add a proper test for the Executor node
    This test makes sure that if any logic that returns false inside of a
    transaction signals back a `false` to whoever called it
    
    No further verification is done for now

--- test/communicator_test.exs
+++ test/communicator_test.exs
@@ -7,6 +7,28 @@ defmodule AnomaTest.Communicator do
 
   alias Anoma.Subscriber.Basic
 
+  alias Anoma.PartialTx
+  alias Anoma.Node.Executor, as: Node
   doctest(Anoma.Node.Executor.Communicator)
 
+  test "Proper Execution" do
+    {:ok, supervisor} = Node.start_link(:p_exec)
+
+    empty_tx = PartialTx.empty()
+
+    successful_tx =
+      empty_tx
+      |> PartialTx.add_input(%Anoma.Resource{quantity: 2, logic: 0})
+
+    failing_tx =
+      empty_tx
+      |> PartialTx.add_input(%Anoma.Resource{quantity: 2, logic: 1})
+
+    assert Communicator.new_transactions(:p_exec_com, [empty_tx])
+    assert Communicator.new_transactions(:p_exec_com, [empty_tx, successful_tx])
+    assert Communicator.new_transactions(:p_exec_com, [failing_tx]) == false
+    assert Communicator.new_transactions(:p_exec_com, [failing_tx, successful_tx]) == false
+
+    Node.shutdown(supervisor)
+  end
 end
