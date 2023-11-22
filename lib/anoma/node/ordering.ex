408c554f1ee6b9ac553036070bb40bac6fc50e1f
Raymond E. Pasco <raymond@heliax.dev>
2023-11-22 10:18:08 -0500

    second demo tx execution

--- lib/anoma/node/ordering.ex
+++ lib/anoma/node/ordering.ex
@@ -10,7 +10,46 @@ defmodule Anoma.Node.Ordering do
   end
 
   def init(_opts) do
-    {:ok, 1}
+    {:ok, %{}}
+  end
+
+  def handle_call(:execute, _from, state) do
+    # order the pending transactions
+    next_order = Anoma.Node.Storage.next_order()
+    ordered_transactions = order(state, next_order)
+    number_of_transactions = length(ordered_transactions)
+    IO.inspect(ordered_transactions, label: "ordered transaction set")
+    :ok = Anoma.Node.Storage.new_order(ordered_transactions)
+
+    for {index, {id, pid}} <- ordered_transactions do
+      IO.inspect(pid, label: "sending write ready to pid")
+      send(pid, {:write_ready, index})
+    end
+
+    {:reply, {:ok, number_of_transactions}, %{}}
+  end
+
+  def handle_call({:execute_manual, tx_list}, _from, state) do
+    next_order = Anoma.Node.Storage.next_order()
+    tx_pid_list = Enum.map(tx_list, fn tx -> spawn_with_random_id(tx) end)
+
+    ordered_transactions =
+      Enum.with_index(tx_pid_list, fn element, index -> {index + next_order, element} end)
+
+    number_of_transactions = length(ordered_transactions)
+    IO.inspect(ordered_transactions, label: "ordered transaction set")
+    :ok = Anoma.Node.Storage.new_order(ordered_transactions)
+
+    for {index, {id, pid}} <- ordered_transactions do
+      IO.inspect(pid, label: "sending write ready to pid")
+      send(pid, {:write_ready, index})
+    end
+
+    {:reply, {:ok, number_of_transactions}, state}
+  end
+
+  def handle_call(:pending, _from, state) do
+    {:reply, {:ok, state}, state}
   end
 
   def handle_call(_msg, _from, state) do
@@ -18,32 +57,57 @@ defmodule Anoma.Node.Ordering do
   end
 
   def handle_cast({:tx, tx_code}, state) do
-    spawn(Anoma.Node.Worker, :run, [state - 1, tx_code])
-    {:noreply, state + 1}
+    random_tx_id = random_id()
+    worker_pid = spawn(Anoma.Node.Worker, :run, [random_tx_id, tx_code])
+    {:noreply, Map.put(state, random_tx_id, worker_pid)}
   end
 
-  def handle_cast({:inject, order, tx_code}, state) do
-    spawn(Anoma.Node.Worker, :run, [order - 1, tx_code])
-    {:noreply, state}
-  end
+  def handle_cast(:reset, state) do
+    for pid <- Map.values(state) do
+      Process.exit(pid, :kill)
+    end
 
-  def handle_cast(:reset, _state) do
-    {:noreply, 1}
+    {:noreply, %{}}
   end
 
   def handle_cast(_msg, state) do
     {:noreply, state}
   end
 
+  def spawn_with_random_id(tx_code) do
+    id = random_id()
+    pid = spawn(Anoma.Node.Worker, :run, [id, tx_code])
+    {id, pid}
+  end
+
+  def order(tx_map, next_order) do
+    Map.to_list(tx_map)
+    |> Enum.shuffle()
+    |> Enum.with_index(fn element, index -> {index + next_order, element} end)
+  end
+
+  def execute() do
+    GenServer.call(__MODULE__, :execute)
+  end
+
+  def execute_manual(tx_list) do
+    GenServer.call(__MODULE__, {:execute_manual, tx_list})
+  end
+
+  def pending() do
+    GenServer.call(__MODULE__, :pending)
+  end
+
   def tx(tx_code) do
     GenServer.cast(__MODULE__, {:tx, tx_code})
   end
 
-  def inject_tx(order, tx_code) do
-    GenServer.cast(__MODULE__, {:inject, order, tx_code})
-  end
-
   def reset() do
     GenServer.cast(__MODULE__, :reset)
+  end
+
+  # 128-bit random id
+  def random_id() do
+    :crypto.strong_rand_bytes(16)
   end
 end
--- lib/anoma/node/worker.ex
+++ lib/anoma/node/worker.ex
@@ -8,17 +8,27 @@ defmodule Anoma.Node.Worker do
   import Nock
 
   def run(order, gate) do
-    IO.inspect(order, label: "worker dispatched after order")
+    IO.inspect(order, label: "worker dispatched with order id")
     {:ok, ordered_tx} = nock(gate, [10, [6, 1 | order], 0 | 1])
     tx_result = nock(ordered_tx, [9, 2, 0 | 1])
 
     case tx_result do
       {:ok, [key | value]} ->
+        IO.inspect(tx_result, label: "worker succeeded with output")
+        # wait to be told to make writes
+        true_order =
+          receive do
+            {:write_ready, order} ->
+              IO.inspect(self(), label: "got write ready")
+              order
+          end
+
+        IO.inspect(true_order, label: "worker writing at true order")
         Anoma.Node.Storage.put(key, value)
-        IO.puts("worker succeeded")
 
       _ ->
         IO.puts("worker failed!")
+        IO.inspect(tx_result, label: "failure result")
     end
   end
 end
