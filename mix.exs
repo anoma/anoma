7019b2848475af0dbb805a450fe637e28980adca
Raymond E. Pasco <raymond@heliax.dev>
2024-01-05 12:42:37 -0500

    Anoma 0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Anoma.MixProject do
   def project do
     [
       app: :anoma,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
