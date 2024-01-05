58855d7960d7999d6065834dbe17a277ba957857
mariari <mariari@protonmail.com>
2024-01-05 12:57:46 +0800

    Add recon tracing to the dependency list
    This will let us use recon trace for tracing our production system

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule Anoma.MixProject do
       {:mnesia_rocksdb, git: "https://github.com/mariari/mnesia_rocksdb"},
       {:typed_struct, "~> 0.3.0"},
       {:xxhash, "~> 0.3"},
+      {:recon, "~> 2.5.4"},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
