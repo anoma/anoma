9664fc7486f8fc8eb7bf1d8c141882958deabbe4
mariari <mariari@protonmail.com>
2024-01-05 12:59:38 +0800

    add rexbug as a tracing dependnecy
    This should also help with tracing, and give it to the ambiant
    environment if anyone so desires to use it

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule Anoma.MixProject do
       {:typed_struct, "~> 0.3.0"},
       {:xxhash, "~> 0.3"},
       {:recon, "~> 2.5.4"},
+      {:rexbug, ">= 2.0.0-rc1"},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
