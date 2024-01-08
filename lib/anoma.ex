d60651023a816368c0deeb2eef7c2069aad127fc
mariari <mariari@protonmail.com>
2024-01-08 14:55:08 +0800

    Turn Anoma into an application
    Here we make an OTP Application.
    
    This has some benefits in :observer.start(), and for making sure that
    our application actually works as expected

--- lib/anoma.ex
+++ lib/anoma.ex
@@ -1,4 +1,6 @@
 defmodule Anoma do
+  use Application
+
   @moduledoc """
   Documentation for `Anoma`.
   """
@@ -14,5 +16,26 @@ defmodule Anoma do
   """
   def hello do
     :world
+  end
+
+  def start(_type, _args) do
+    storage = %Anoma.Storage{
+      qualified: Anoma.Qualified,
+      order: Anoma.Order
+    }
+
+    name = :anoma
+    snapshot_path = [:my_special_nock_snaphsot | 0]
+
+    children = [
+      {Anoma.Node,
+       name: name,
+       snapshot_path: snapshot_path,
+       storage: storage,
+       block_storage: :anoma_block},
+      {Anoma.Node.Intent, :anoma_intent}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one, name: Anoma)
   end
 end
