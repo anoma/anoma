30722b649f06bd42f9253971f087f4675c846fd1
mariari <mariari@protonmail.com>
2024-01-10 09:38:07 +0800

    Add ExDoc generation to the codebase
    This lets us run:
    
    mix docs
    
    which deploys a doc/index.html which is the main page of the project
    
    Further we create a stub docs and extras section for further development

--- mix.exs
+++ mix.exs
@@ -12,7 +12,10 @@ defmodule Anoma.MixProject do
         plt_local_path: "plts/anoma.plt",
         plt_core_path: "plts/core.plt",
         flags: ["-Wno_improper_lists"]
-      ]
+      ],
+      # Docs
+      name: "Anoma",
+      docs: docs()
     ]
   end
 
@@ -39,9 +42,20 @@ defmodule Anoma.MixProject do
       {:xxhash, "~> 0.3"},
       {:recon, "~> 2.5.4"},
       {:rexbug, ">= 2.0.0-rc1"},
+      {:ex_doc, "~> 0.31", only: [:dev], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp docs do
+    [
+      extras: extras()
+    ]
+  end
+
+  def extras() do
+    []
   end
 end
