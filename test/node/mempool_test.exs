350b56a70578a2a802e13941eec55224078ee93f
Artem Gureev <artemgureev@yahoo.com>
2024-01-16 23:06:22 +0700

    Add Test for Pending Transactions Printing
    Adds a test for printing transactions.

--- test/node/mempool_test.exs
+++ test/node/mempool_test.exs
@@ -86,4 +86,14 @@ defmodule AnomaTest.Node.Mempool do
     assert not Process.alive?(pid_zero)
     assert :absent = Storage.get(storage, key)
   end
+
+  test "Transaction print", %{node: node} do
+    key = 666
+    Ccom.subscribe(node.executor, self())
+    Mcom.hard_reset(node.mempool)
+
+    zero_tx = Mcom.tx(node.mempool, zero_counter(key))
+    assert Mcom.pending_txs(node.mempool) == [zero_tx]
+    Mcom.soft_reset(node.mempool)
+  end
 end
