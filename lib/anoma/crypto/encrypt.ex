4903465d274b7040909f7fe746764359d2185bc0
mariari <mariari@protonmail.com>
2024-02-02 15:56:57 +0800

    Add an Encryption module
    This allows us to generate out keys, and encrypt messages to a person

---
+++ lib/anoma/crypto/encrypt.ex
@@ -0,0 +1,30 @@
+defmodule Anoma.Crypto.Encrypt do
+  @moduledoc false
+
+  @type box_public() :: <<_::256>>
+  @type box_secret() :: <<_::256>>
+
+  @spec new_keypair() :: %{public: box_public(), secret: box_secret()}
+  def new_keypair() do
+    :enacl.box_keypair()
+  end
+
+  @doc """
+  I seal the given message for the publicly known recipient.
+
+  The message will be turned into binary via :erlang.term_to_binary,
+  so please do not turn it to binary before hand.
+  """
+  @spec seal(any(), box_public()) :: binary()
+  def seal(message, pub) do
+    :enacl.box_seal(:erlang.term_to_binary(message), pub)
+  end
+
+  @spec unseal(binary(), box_public(), box_secret()) ::
+          {:ok, any()} | {:error, :failed_verification}
+  def unseal(cipher, pub, sec) do
+    with {:ok, encrypt} <- :enacl.box_seal_open(cipher, pub, sec) do
+      {:ok, :erlang.binary_to_term(encrypt)}
+    end
+  end
+end
