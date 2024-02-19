f0531db713e6daded84ee2e554b733da0d9fc98d
mariari <mariari@protonmail.com>
2024-02-19 16:54:53 +0800

    Create tests that properly reflect the new verification API
    We create some test examples that properly test if the verification
    works as intended

--- test/identity/commitment_test.exs
+++ test/identity/commitment_test.exs
@@ -16,9 +16,19 @@ defmodule AnomaTest.Identity.Commitment do
     {:ok, data} = Commitment.commit(cpid, 555)
 
     assert Verification.verify_request(data, 555, pair.external)
+    refute Verification.verify_request(data, <<555>>, pair.external)
   end
 
-  test "Verifying fails on unrelated data" do
+  test "We properly get back the given binary in combined mode" do
+    pair = Id.new_keypair()
+    encapsulated = {pair.internal.sign, :ed25519}
+    {:ok, cpid} = Commitment.start_link(encapsulated)
+
+    {:ok, data} = Commitment.commit_combined(cpid, <<55>>)
+    assert {:ok, <<55>>} == Verification.verify_combined(data, pair.external)
+  end
+
+  test "Verifying fails on unrelated data, combined" do
     pair = Id.new_keypair()
     refute Verification.verify_combined(<<5>>, pair.external)
   end
