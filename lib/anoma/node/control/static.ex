c56d6c3f04df63d3537272878a2061dcf05a9307
Artem Gureev <artemgureev@yahoo.com>
2024-02-21 20:45:38 +0700

    Add Initial Implementation of the Static Configuration Engine
    Adds the initial implementation of the Static Configuration Engine as
    part of the Control Machine.
    
    Currently implemented as a module with apppropriate `get/2` function
    rather than a genserver.
    
    Key submission is naive.

---
+++ lib/anoma/node/control/static.ex
@@ -0,0 +1,13 @@
+defmodule Anoma.Node.Control.Static do
+  @moduledoc """
+  I implement the static configuration engine.
+
+  The user can query to ask the value of the configuration key relative to the storage.
+  """
+
+  alias Anoma.Storage
+
+  def get(storage, key) do
+    {key, Storage.get(storage, key)}
+  end
+end
