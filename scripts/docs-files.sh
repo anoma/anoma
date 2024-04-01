365cc121e18b75c1ef1e8e8abf6a333559529601
mariari <mariari@protonmail.com>
2024-04-01 14:40:57 +0800

    Create a script to include files to the generated documentation website

---
+++ scripts/docs-files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Destination folder where all files will be copied
+destination_folder="./doc/files/"
+
+mkdir $destination_folder
+# Find all folders named "files" and copy their contents to the destination folder
+find ./documentation -type d -name "files" -exec sh -c 'cp -r "$0"/* "$1"' {} "$destination_folder" \;
+
+echo "All files copied and merged into $destination_folder"
