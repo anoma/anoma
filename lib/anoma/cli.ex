0de45722bb3381067c3828b28ad90bf7bac79eaa
mariari <mariari@protonmail.com>
2024-04-03 12:07:53 +0800

    Properly trigger the help options
    Help on subcommands give a special result. Rather than redoing work,
    we should just reuse parse! if we are down one of these paths

--- lib/anoma/cli.ex
+++ lib/anoma/cli.ex
@@ -38,8 +38,6 @@ defmodule Anoma.Cli do
     []
   end
 
-  # Optimus.t() is opaque so the help fails to type check, but it's OK
-  @dialyzer {:nowarn_function, start_application: 1}
   @spec start_application([String.t()]) :: {:ok, pid()} | {:error, any()}
   def start_application(arguments) do
     start_anoma = fn parsed ->
@@ -62,12 +60,22 @@ defmodule Anoma.Cli do
         System.halt(0)
 
       :help ->
-        IO.puts(Optimus.help(Anoma.Cli.argument_parser()))
-        System.halt(0)
+        # The default parse! will quit
+        Optimus.parse!(Anoma.Cli.argument_parser(), arguments)
+
+      # Happens on sub commands as well
+      {:help, _} ->
+        Optimus.parse!(Anoma.Cli.argument_parser(), arguments)
 
       _ ->
-        IO.puts(Optimus.help(Anoma.Cli.argument_parser()))
+        top_level_help()
         System.halt(1)
     end
+  end
+
+  # Optimus.t() is opaque so the help fails to type check, but it's OK
+  @dialyzer {:nowarn_function, top_level_help: 0}
+  def top_level_help() do
+    IO.puts(Optimus.help(Anoma.Cli.argument_parser()))
   end
 end
