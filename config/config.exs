e4488ea6f0635a10b22f903bea2fcb0100d5a89d
Artem Gureev <artemgureev@yahoo.com>
2024-04-05 20:59:05 +0700

    Add Home Directory Configuration
    Adds Anoma Home Directory configuration to the Anoma configuration
    file. We follow XDG format and create directories for data, config,
    and state appropriately, either by reading from the environment or by
    setting defaults.
    
    The directory names are created as `anoma_name` where `name` is the
    name of the environment Anoma was launched in.
    
    Sets mnesia directory to the data directory.

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,23 @@ config :logger,
   handle_otp_reports: true,
   handle_sasl_reports: true
 
+app = ~c"anoma_#{Mix.env()}"
+
+data = :filename.basedir(:user_data, app) |> List.to_string()
+config = :filename.basedir(:user_config, app) |> List.to_string()
+env = config_env()
+
 config :anoma,
-  env: config_env()
+  env: env,
+  data: data,
+  config: config
+
+unless env == :test do
+  File.mkdir_p!(data)
+  File.mkdir_p!(config)
+
+  config :mnesia,
+    dir: ~c"#{data}"
+end
 
 import_config "#{config_env()}.exs"
