feb8a994fee2e3ecf3b4e8aaec3c93efe80a465d
mariari <mariari@protonmail.com>
2024-05-04 17:11:24 +0800

    Add Calculating of a layer
    We add the ability to calculate the mug of a given layer. See the
    documentation in this commit for how it works

--- lib/nock/jets.ex
+++ lib/nock/jets.ex
@@ -39,14 +39,60 @@ defmodule Nock.Jets do
 
   """
   def calculate_mug_of_core(index_in_core, parent_layer) do
-    {:ok, core} =
-      Nock.nock(Nock.logics_core(), [
-        8,
-        [9, index_in_core, 0 | Noun.index_to_offset(parent_layer)],
-        0 | 2
-      ])
-
+    {:ok, core} = calculate_core(index_in_core, parent_layer)
     Noun.mug(hd(core))
+  end
+
+  @doc """
+  We calculate the mug of a given layer at any given gate.
+
+  ### Parameters
+
+  - `index_in_core` - the index of the gate whose parents we wish to
+    learn about
+
+  - `parent_layer` - the layer of the standard library. This should be
+    the same as the numbers found in `anoma.hoon` with `+ 3` added for
+    `@rm_core_val`, `@stdlib_core_val`, and the current index being
+    pushed
+
+  ### Example
+
+  In the Hoon repl one should write
+
+      dojo> |commit %anoma
+      >=
+      dojo> =anoma -build-file /=anoma=/lib/anoma/hoon
+      dojo> =>  anoma  !=(sub)
+      [9 47 0 31]
+
+  Now in IEX
+
+      > Nock.Jets.calculate_mug_of_layer(47, 7)
+      17654928022549292273
+
+  We derived the 7 because the current top layer is 5, and dec is at
+  layer 1. So 5 - 1 + 3 = 7
+
+  This value should match Nock.@layer_1_contex_mug
+  """
+  @spec calculate_mug_of_layer(non_neg_integer(), non_neg_integer()) ::
+          non_neg_integer()
+  def calculate_mug_of_layer(index_in_core, parent_layer) do
+    with {:ok, core} <- calculate_core(index_in_core, parent_layer),
+         {:ok, parent} <- Noun.axis(7, core) do
+      Noun.mug(parent)
+    end
+  end
+
+  @spec calculate_core(non_neg_integer(), non_neg_integer()) ::
+          :error | {:ok, Noun.t()}
+  defp calculate_core(index_in_core, parent_layer) do
+    Nock.nock(Nock.logics_core(), [
+      8,
+      [9, index_in_core, 0 | Noun.index_to_offset(parent_layer)],
+      0 | 2
+    ])
   end
 
   # when this is called, we've already jet-matched axis 7.
