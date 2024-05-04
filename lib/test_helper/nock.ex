7726cc108eaf3528b205d9018d2311f51e9e10f0
mariari <mariari@protonmail.com>
2024-05-04 15:47:49 +0800

    Add the generated nock logic to the standard library
    We update all references that referred to the core index. Everything
    gets bumped by (+ 1 (* 2 x)), hence the changes observed
    
    Furhter we bump the number of layers from 5 to 6 to reflect this

--- lib/test_helper/nock.ex
+++ lib/test_helper/nock.ex
@@ -8,7 +8,7 @@ defmodule TestHelper.Nock do
 
   @spec using_dec_core() :: Noun.t()
   def using_dec_core() do
-    arm = Noun.Format.parse_always("[8 [9 342 0 511] 9 2 10 [6 0 14] 0 2]")
+    arm = Noun.Format.parse_always("[8 [9 342 0 1.023] 9 2 10 [6 0 14] 0 2]")
     sample = 999
     [arm, sample | logics_core()]
   end
@@ -26,9 +26,9 @@ defmodule TestHelper.Nock do
         9
         2
         10
-        [30 8 [9 342 0 2.047] 9 2 10 [6 0 62] 0 2]
+        [30 8 [9 342 0 4.095] 9 2 10 [6 0 62] 0 2]
         10
-        [6 [8 [9 20 0 2.047] 9 2 10 [6 [0 29] 0 28] 0 2] 0 12]
+        [6 [8 [9 20 0 4.095] 9 2 10 [6 [0 29] 0 28] 0 2] 0 12]
         0
         1
       ]
