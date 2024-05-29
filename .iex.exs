5325dcd3efbdd89b57ae202db27911f7f4bf29f1
Artem Gureev <artemgureev@yahoo.com>
2024-05-29 20:44:21 +0700

    Create Local `.iex.exs` File
    Creates a local `.iex.exs` file for the Anoma repo. It aliases all
    core modules and engines commonly used in interactive environment and
    imports the global user config if availiable.

---
+++ .iex.exs
@@ -0,0 +1,5 @@
+alias Anoma.{Node, Block, Dump, Mnesia}
+alias Node.{Router, Transport, Ordering, Executor, Mempool, Pinger, Clock, Logger, Storage}
+alias Router.Engine
+
+import_file_if_available("~/.iex.exs")
