a1d3c7e82c76b56bd5696e573434882557edb57d
mariari <mariari@protonmail.com>
2024-06-17 17:35:01 +0800

    Made `Anoma.Dump.launch/3` keyword based
    Before it just accepted a supervisor name, I wish to extend this in a
    later commit to contain if we are in a testing environment or not, so
    this is the first step towards new functionality

--- lib/anoma.ex
+++ lib/anoma.ex
@@ -47,7 +47,7 @@ defmodule Anoma do
     dump_path = Configuration.locate_dump_file(config)
 
     if dump_path do
-      Anoma.Dump.launch(dump_path, :anoma, Anoma)
+      Anoma.Dump.launch(dump_path, :anoma, supervisor: [name: Anoma])
     else
       Configuration.launch_min(config, rocks_flag, Anoma)
     end
