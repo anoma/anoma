13a23ba19c92cc6a2c15514048f119945b4dbcfa
mariari <mariari@protonmail.com>
2024-06-19 16:43:52 +0800

    Convert Resource Tests to Resource Examples
    In the process we've actually made a few more tests!
    
    1. we added an empty transaction test
    2. We've added some more kind tests
    3. We've removed redundant checks
    4. We've add a proof record test
    
    Further we've add the following features to the test
    
    1. We've reduced the number of random keys we generate, and make them
       persistent
    2. We've reused a lot of resources. Before they all had to be made
       from scratch. No more!
    3. We utilized new helpers to create unique resources.
    4. We've reused transactions! We can incrementally add and fix
       transactions. No need to recreate from scratch!
    5. We made top level kinds and delta balances, which were missing
       before

---
+++ lib/examples/eproof_record.ex
@@ -0,0 +1,35 @@
+defmodule Examples.EProofRecord do
+  use Memoize
+
+  require ExUnit.Assertions
+  import ExUnit.Assertions
+
+  alias Anoma.Resource.ProofRecord
+
+  alias Examples.EResource
+
+  # Any interesting properties to show?
+  @spec a10_space_proof() :: ProofRecord.t()
+  def a10_space_proof() do
+    resource = ProofRecord.prove(EResource.a10_space_resource())
+    assert resource == ProofRecord.from_noun(ProofRecord.to_noun(resource))
+    resource
+  end
+
+  @spec b10_space_proof() :: ProofRecord.t()
+  @spec a5_space_proof() :: ProofRecord.t()
+  @spec a10_d0_proof() :: ProofRecord.t()
+  @spec b10_d0_proof() :: ProofRecord.t()
+  def b10_space_proof(), do: ProofRecord.prove(EResource.b10_space_resource())
+  def a5_space_proof(), do: ProofRecord.prove(EResource.a5_space_resource())
+  def a10_d0_proof(), do: ProofRecord.prove(EResource.a10_d0_resource())
+  def b10_d0_proof(), do: ProofRecord.prove(EResource.b10_d0_resource())
+
+  @spec a0_counter_proof() :: ProofRecord.t()
+  def a0_counter_proof(),
+    do: ProofRecord.prove(EResource.a0_counter_resource())
+
+  @spec a1_counter_proof() :: ProofRecord.t()
+  def a1_counter_proof(),
+    do: ProofRecord.prove(EResource.a1_counter_resource())
+end
