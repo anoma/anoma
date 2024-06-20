49c84ca0827a44d042d81d2061c259faceb02129
mariari <mariari@protonmail.com>
2024-06-20 12:39:11 +0800

    Move Crypto tests to a signular file
    Both the sub folders have only 1 test in them. In the future these
    will be converted to examples, making the extra files a burden

--- test/crypto_test.exs
+++ test/crypto_test.exs
@@ -1,10 +1,17 @@
-defmodule AnomaTest.Crypto.Id do
+defmodule AnomaTest.Crypto do
   use ExUnit.Case, async: true
 
   alias Anoma.Crypto.Id
   alias Anoma.Crypto.Symmetric
 
   doctest(Anoma.Crypto.Id)
+
+  test "decrypt · encrypt ≡ identity" do
+    xcc = Symmetric.random_xchacha()
+
+    assert 555 ==
+             555 |> Symmetric.encrypt(xcc) |> Symmetric.decrypt(xcc)
+  end
 
   test "unsalt · salt ≡ identity" do
     xcc = Symmetric.random_xchacha()
