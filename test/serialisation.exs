ad1bb7bb1ce629b12220b534832d56ce799977d0
mariari <mariari@protonmail.com>
2024-06-27 10:19:43 +0800

    Move the serialisation test to being an example
    We improve the checks a bit, by checking the term and the pack and
    unpacked term are the same when unserialised, since they are not the
    same themselves

--- test/serialisation.exs
+++ test/serialisation.exs
@@ -1,24 +1,11 @@
 defmodule SerialisationTest do
+  alias Examples.ESerialisation
   use TestHelper.TestMacro
   doctest Anoma.Serialise
 
-  test "basic" do
-    # test from_msgpack o to_msgpack = id
-    tid = fn x ->
-      x ===
-        elem(
-          Anoma.Serialise.from_msgpack(
-            elem(
-              :msgpack.unpack(:msgpack.pack(Anoma.Serialise.to_msgpack(x))),
-              1
-            )
-          ),
-          1
-        )
-    end
-
-    assert tid.(%Anoma.Resource.Transaction{})
-    assert tid.(Anoma.Resource.new())
-    assert tid.(%{[1, 2 | 3] => 5})
+  test "examples" do
+    ESerialisation.empty_tx()
+    ESerialisation.simple_map()
+    ESerialisation.aresource()
   end
 end
