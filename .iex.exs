9ea7d22b61ea0f01f243e0af2d2c6a87ef93331d
mariari <mariari@protonmail.com>
2024-07-04 13:45:28 +0800

    Alias Examples

--- .iex.exs
+++ .iex.exs
@@ -1,5 +1,38 @@
 alias Anoma.{Node, Block, Dump, Mnesia}
-alias Node.{Router, Transport, Ordering, Executor, Mempool, Pinger, Clock, Logger, Storage}
+
+alias Node.{
+  Router,
+  Transport,
+  Ordering,
+  Executor,
+  Mempool,
+  Pinger,
+  Clock,
+  Logger,
+  Storage
+}
+
 alias Router.Engine
+
+alias Examples.{
+  EBlock,
+  ECommitmentTree,
+  EConfiguration,
+  EIdentity,
+  ENock,
+  ENode,
+  EProofRecord,
+  EResource,
+  ESerialisation,
+  ETransaction
+}
+
+alias Examples.ENode.{
+  EClock,
+  EDumper,
+  EIntent,
+  EPinger,
+  EStorage
+}
 
 import_file_if_available("~/.iex.exs")
