97f103c30956d34d8adb10c03f9b61939d7e0bea
Aleksandr Karbyshev <karbyshev@users.noreply.github.com>
2024-07-24 16:35:35 +0200

    Factor out submit command example

--- lib/examples/eparser.ex
+++ lib/examples/eparser.ex
@@ -21,14 +21,36 @@ defmodule Examples.EParser do
 
   @type zero_submit_423() :: {[Cli.client_commands(), ...], parse_result()}
   def zero_submit_423() do
-    file = "./test/data/zero-423.nock"
-    seeded_argument = ["submit", file]
+    zero_nock = "./test/data/zero-423.nock"
+    submit_cmd(zero_nock, false)
+  end
+
+  ####################################################################
+  ##                             Helpers                            ##
+  ####################################################################
+
+  @spec submit_cmd(String.t(), boolean()) ::
+          {[Cli.client_commands(), ...], parse_result()}
+  defp submit_cmd(filename, ro) do
+    submit =
+      if ro do
+        "ro-submit"
+      else
+        "submit"
+      end
+
+    command_line = [submit, filename]
 
     assert {:ok, cmd, res} =
-             Optimus.parse(Anoma.Cli.argument_parser(), seeded_argument)
+             Optimus.parse(Anoma.Cli.argument_parser(), command_line)
 
-    assert [:submit] == cmd
-    assert %{file: file} == res.args
+    if ro do
+      assert [:ro_submit] == cmd
+    else
+      assert [:submit] == cmd
+    end
+
+    assert %{file: filename} == res.args
     {cmd, res}
   end
 end
