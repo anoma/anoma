fc980aa44383df93207c896ad8c10c47fcbb7356
mariari <mariari@protonmail.com>
2024-07-25 09:59:43 +0800

    Update Resource to fulfill the Nounable interfaces

--- lib/anoma/resource/proof_record.ex
+++ lib/anoma/resource/proof_record.ex
@@ -20,15 +20,17 @@ defmodule Anoma.Resource.ProofRecord do
 
   @spec to_noun(t()) :: Noun.t()
   def to_noun(record) do
-    Resource.to_noun(record.proof.resource)
+    Noun.Nounable.to_noun(record.proof.resource)
   end
 
   @spec from_noun(Noun.t()) :: t()
   def from_noun(record) do
-    %ProofRecord{
-      proof: %Proof{
-        resource: Resource.from_noun(record)
+    with {:ok, resource} <- Resource.from_noun(record) do
+      %ProofRecord{
+        proof: %Proof{
+          resource: resource
+        }
       }
-    }
+    end
   end
 end
