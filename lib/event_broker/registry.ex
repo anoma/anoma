d5503b9ff525f475966ce771c3cff207c2bf9b39
agureev <artguril@proton.me>
2024-07-29 21:06:50 +0400

    Abstract the Subscribe Logic For Registry

--- lib/event_broker/registry.ex
+++ lib/event_broker/registry.ex
@@ -45,6 +45,8 @@ defmodule EventBroker.Registry do
   ############################################################
 
   def handle_call({:subscribe, pid, filter_spec_list}, _from, state) do
+    registered = state.registered_filters
+
     invalid_filters =
       filter_spec_list
       |> Enum.flat_map(fn spec ->
@@ -59,39 +61,11 @@ defmodule EventBroker.Registry do
 
     if Enum.empty?(invalid_filters) do
       new_state =
-        if Map.has_key?(state.registered_filters, filter_spec_list) do
+        if Map.has_key?(registered, filter_spec_list) do
           state
         else
-          existing_prefix =
-            state.registered_filters
-            |> Map.keys()
-            |> Enum.filter(fn p -> List.starts_with?(filter_spec_list, p) end)
-            |> Enum.sort(&(length(&1) > length(&2)))
-            |> hd()
-
-          remaining_to_spawn = filter_spec_list -- existing_prefix
-
-          # ["a", "b"] -> ["a", "b", "c", "d"]
-          # {["a", "b"], state} iterating over "c"
-          # {["a", "b", "c"], state + ["a", "b", "c"] iterating over "d"
-
           {_, new_registered_filters} =
-            for f <- remaining_to_spawn,
-                reduce: {existing_prefix, state.registered_filters} do
-              {parent_spec_list, old_state} ->
-                parent_pid = Map.get(old_state, parent_spec_list)
-
-                {:ok, new_pid} =
-                  GenServer.start_link(
-                    EventBroker.FilterAgent,
-                    f
-                  )
-
-                GenServer.call(parent_pid, {:subscribe, new_pid})
-                new_spec_list = parent_spec_list ++ [f]
-                new_state = Map.put(old_state, new_spec_list, new_pid)
-                {new_spec_list, new_state}
-            end
+            iterate_sub(registered, filter_spec_list)
 
           %{state | registered_filters: new_registered_filters}
         end
@@ -137,10 +111,39 @@ defmodule EventBroker.Registry do
     {:noreply, %{state | registered_filters: Map.delete(filters, key)}}
   end
 
-  def find_filter_info(filters, filter) do
-    filters
-    |> Enum.find(fn {key, _val} ->
-      List.last(key) == filter
-    end)
+  ############################################################
+  #                          Helpers                         #
+  ############################################################
+
+  defp iterate_sub(registered, filter_spec_list) do
+    existing_prefix =
+      registered
+      |> Map.keys()
+      |> Enum.filter(fn p -> List.starts_with?(filter_spec_list, p) end)
+      |> Enum.sort(&(length(&1) > length(&2)))
+      |> hd()
+
+    remaining_to_spawn = filter_spec_list -- existing_prefix
+
+    # ["a", "b"] -> ["a", "b", "c", "d"]
+    # {["a", "b"], state} iterating over "c"
+    # {["a", "b", "c"], state + ["a", "b", "c"] iterating over "d"
+
+    for f <- remaining_to_spawn,
+        reduce: {existing_prefix, registered} do
+      {parent_spec_list, old_state} ->
+        parent_pid = Map.get(old_state, parent_spec_list)
+
+        {:ok, new_pid} =
+          GenServer.start_link(
+            EventBroker.FilterAgent,
+            f
+          )
+
+        GenServer.call(parent_pid, {:subscribe, new_pid})
+        new_spec_list = parent_spec_list ++ [f]
+        new_state = Map.put(old_state, new_spec_list, new_pid)
+        {new_spec_list, new_state}
+    end
   end
 end
