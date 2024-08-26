8a1b1d2eadeea678f6385b62a485376f15801b25
mariari <mariari@protonmail.com>
2024-08-26 14:43:37 +0800

    Add the ability to get the TCP listener from a given TCPServer
    There were no comamnds to expose the Port.
    
    This is a nice feature as this allows us to test various conditions:
    
    1. We would like to test shutting down the port, we don't have this in
       yet, but provides a good way of seeing system behavior. Further we
       should handle unexpected closes.

--- lib/anoma/node/transport/tcp_server.ex
+++ lib/anoma/node/transport/tcp_server.ex
@@ -67,6 +67,17 @@ defmodule Anoma.Node.Transport.TCPServer do
   end
 
   ############################################################
+  #                      Public RPC API                      #
+  ############################################################
+
+  @doc """
+  I expose the listener of the server
+  """
+  def listener(server) do
+    Router.call(server, :listener)
+  end
+
+  ############################################################
   #                    Genserver Behavior                    #
   ############################################################
 
@@ -79,6 +90,10 @@ defmodule Anoma.Node.Transport.TCPServer do
     )
 
     {:noreply, s}
+  end
+
+  def handle_call(:listener, _from, s) do
+    {:reply, s.listener, s}
   end
 
   ############################################################
