f2dd748bb16fd81d339c2946656d878c8d5ffba9
juped <juped@users.noreply.github.com>
2024-08-27 19:45:38 +0000

    deploy: a21f4036f9434965c46590d473bcaf0db03a594f

--- .doc-versions.js
+++ .doc-versions.js
@@ -6,6 +6,7 @@ var versionNodes = [
 {version: "v0.5.0", url: "https://anoma.github.io/anoma/v0.5.0/"},
 {version: "v0.4.0", url: "https://anoma.github.io/anoma/v0.4.0/"},
 {version: "v0.3.0", url: "https://anoma.github.io/anoma/v0.3.0/"},
+{version: "v0.20.0", url: "https://anoma.github.io/anoma/v0.20.0/"},
 {version: "v0.2.0", url: "https://anoma.github.io/anoma/v0.2.0/"},
 {version: "v0.19.1", url: "https://anoma.github.io/anoma/v0.19.1/"},
 {version: "v0.19.0", url: "https://anoma.github.io/anoma/v0.19.0/"},
