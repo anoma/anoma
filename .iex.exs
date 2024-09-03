2914a039911b2c3a38081a5f9bfa8dc737b445e0
mariari <mariari@protonmail.com>
2024-09-03 17:07:14 +0800

    Alias all the example modules

--- .iex.exs
+++ .iex.exs
@@ -16,23 +16,28 @@ alias Router.Engine
 
 alias Examples.{
   EBlock,
+  EClient,
   ECommitmentTree,
   EConfiguration,
+  ECrypto,
   EIdentity,
   ENock,
   ENode,
+  EParser,
   EProofRecord,
   EResource,
   ESerialisation,
-  ETransaction
+  ETransaction,
 }
 
 alias Examples.ENode.{
   EClock,
   EDumper,
   EIntent,
+  EMempool,
   EPinger,
-  EStorage
+  EStorage,
+  ETransport.ETCP
 }
 
 import_file_if_available("~/.iex.exs")
