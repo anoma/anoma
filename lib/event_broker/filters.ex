2e77a0aa5574b7bfa5cf3d77cc6d55978cad7601
Raymond E. Pasco <raymond@heliax.dev>
2024-09-09 06:19:36 +0200

    event broker: add an example filter with more fields
    This is to better serve as a longwinded example of the deffilter macro
    in action, using all its features.

--- lib/event_broker/filters.ex
+++ lib/event_broker/filters.ex
@@ -15,6 +15,25 @@ defmodule EventBroker.Filters do
     value
   end
 
+  deffilter ManyFields,
+    event: %EventBroker.Event{
+      source_module: event_module,
+      body: {event_foo, event_bar}
+    },
+    params: %ManyFields{
+      module: params_module,
+      foo: params_foo,
+      bar: params_bar
+    },
+    fields: [
+      field(:module, module()),
+      field(:foo, term()),
+      field(:bar, term())
+    ] do
+    event_module == params_module && event_foo == params_foo &&
+      event_bar == params_bar
+  end
+
   deffilter SourceModule,
     event: %EventBroker.Event{source_module: event_module},
     params: %SourceModule{module: params_module},
