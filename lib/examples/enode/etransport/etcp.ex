7d1039e756e17582ab1bd499470745660097e08f
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-09-18 11:52:45 +0200

    partial update to the example

--- lib/examples/enode/etransport/etcp.ex
+++ lib/examples/enode/etransport/etcp.ex
@@ -1,10 +1,13 @@
 defmodule Examples.ENode.ETransport.ETCP do
   use Memoize
 
-  alias Anoma.Node.Transport
-  alias Examples.ENode
-  alias Anoma.Node.Transport2.Router
+  import ExUnit.Assertions
 
+  alias Anoma.Node.Transport
+  alias Anoma.Node.Transport2.Router
+  alias Examples.ENode
+
+  require ExUnit.Assertions
   require ExUnit.Assertions
 
   ############################################################
@@ -16,7 +19,26 @@ defmodule Examples.ENode.ETransport.ETCP do
     # in its config
     # note: if you want to change/fix this port, you have to change this in the config
     #       in anoma.ex
-    Router.start_tcp_server()
+    :ok = Router.start_tcp_server()
+
+    IO.puts("""
+    startup a second node to connect to this node:
+
+    `iex --name node_b@127.0.0.1 --cookie mycookie -S mix`
+
+    and run the following command:
+
+    `Examples.ENode.ETransport.ETCP.test_tcp_server_2`
+
+
+    Press any key to continue
+    """)
+
+    IO.read(:stdio, :line)
+
+    # there should be a proxy to the router in the registry
+    # and a tcp connection
+    assert Enum.count(Router.dump_register()) == 2
   end
 
   def test_tcp_server_2() do
