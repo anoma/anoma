ca27fcb81636d485dfa529a35a4da1c5b1218b6a
mariari <mariari@protonmail.com>
2024-09-23 10:26:44 +0800

    Move EventBroker Tests to the EventBroker app
    Before it was being ran under the anoma_lib app, which is incorrect,
    this commit corrects this mistake

--- apps/event_broker/lib/examples/e_event_broker.ex
+++ apps/event_broker/lib/examples/e_event_broker.ex
@@ -1,7 +1,7 @@
-defmodule Examples.EventBroker do
+defmodule Examples.EEventBroker do
   @moduledoc false
 
-  use TestHelper.TestMacro
+  use ExUnit.Case
   alias EventBroker.{Supervisor, Registry, Filters, Event}
 
   @doc """
@@ -420,6 +420,6 @@ defmodule Examples.EventBroker do
     unsub_all()
 
     assert "[Nock] do not export filtering functions" ==
-             EventBroker.subscribe_me([%Nock{}])
+             EventBroker.subscribe_me([%{__struct__: Nock}])
   end
 end
