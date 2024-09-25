b0e6997ca100afb9a12f61131ca69ecc90bcedc4
agureev <artguril@proton.me>
2024-09-25 15:00:45 +0400

    Add Logging Examples

---
+++ apps/anoma_node/test/logging_test.exs
@@ -0,0 +1,19 @@
+defmodule LoggingTest do
+  use ExUnit.Case, async: false
+
+  alias Anoma.Node.Examples.ELogging
+
+  test "transaction event examples" do
+    ELogging.check_tx_event()
+    ELogging.check_multiple_tx_events()
+  end
+
+  test "consensus event examples" do
+    ELogging.check_consensus_event()
+  end
+
+  test "block event examples" do
+    ELogging.check_block_event()
+    ELogging.check_block_event_leave_one_out()
+  end
+end
