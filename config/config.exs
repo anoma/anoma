52e2d5359f2a5958e91138675981070b458f1518
agureev <artguril@proton.me>
2024-09-25 17:22:38 +0400

    Add Logging Configuration
    Adds Logging configuration to be on the `:error` level by default

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,10 @@
 import Config
 
+config :logger,
+  level: :error,
+  handle_otp_reports: true,
+  handle_sasl_reports: true
+
 config :anoma_lib, []
 config :anoma_node, []
 config :event_broker, []
