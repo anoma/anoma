b627df06eb075b9d35535a37bea3dc770ef630a8
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-10-10 15:43:48 +0200

    reconstruct enode if not found, but already started

--- apps/anoma_node/lib/examples/e_node.ex
+++ apps/anoma_node/lib/examples/e_node.ex
@@ -68,13 +68,9 @@ defmodule Anoma.Node.Examples.ENode do
     enode =
       case Anoma.Supervisor.start_node(opts) do
         {:ok, pid} ->
-          enode = %ENode{
-            node_id: opts[:node_id],
-            pid: pid,
-            tcp_ports: []
-          }
-
+          enode = %ENode{node_id: opts[:node_id], pid: pid, tcp_ports: []}
           :ets.insert(@table_name, {pid, enode})
+
           enode
 
         {:error, {:already_started, pid}} ->
