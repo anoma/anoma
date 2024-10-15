2e05ca1710897a15d0a28c116c1f68b86dfc3909
mariari <mariari@protonmail.com>
2024-10-15 15:19:53 +0800

    Type the executor module

--- apps/anoma_node/lib/node/transaction/executor.ex
+++ apps/anoma_node/lib/node/transaction/executor.ex
@@ -9,6 +9,8 @@ defmodule Anoma.Node.Transaction.Executor do
 
   require EventBroker.Event
 
+  @typep startup_options() :: {:node_id, String.t()}
+
   typedstruct do
     field(:node_id, String.t())
   end
@@ -17,13 +19,14 @@ defmodule Anoma.Node.Transaction.Executor do
     field(:result, list({:ok | :error, binary()}))
   end
 
+  @spec start_link([startup_options()]) :: GenServer.on_start()
   def start_link(args) do
     args = Keyword.validate!(args, [:node_id])
     name = Registry.name(args[:node_id], __MODULE__)
     GenServer.start_link(__MODULE__, args, name: name)
   end
 
-  @spec init(any()) :: {:ok, Executor.t()}
+  @spec init([startup_options()]) :: {:ok, Executor.t()}
   def init(args) do
     Process.set_label(__MODULE__)
 
@@ -36,11 +39,13 @@ defmodule Anoma.Node.Transaction.Executor do
     {:ok, state}
   end
 
+  @spec launch(String.t(), {Backends.backend(), Noun.t()}, binary()) :: :ok
   def launch(node_id, tw_w_backend, id) do
     pid = Registry.whereis(node_id, __MODULE__)
     GenServer.cast(pid, {:launch, tw_w_backend, id})
   end
 
+  @spec execute(String.t(), [binary()]) :: :ok
   def execute(node_id, consensus) do
     pid = Registry.whereis(node_id, __MODULE__)
     GenServer.cast(pid, {:execute, consensus})
@@ -77,6 +82,7 @@ defmodule Anoma.Node.Transaction.Executor do
     {:noreply, state}
   end
 
+  @spec execution_event([{any(), binary()}]) :: :ok
   def execution_event(res_list) do
     event =
       EventBroker.Event.new_with_body(%__MODULE__.ExecutionEvent{
