ac0ac6af14385f8e59507771d856d2b512057b4a
mariari <mariari@protonmail.com>
2024-10-15 21:41:38 +0800

    Update the formatter to ignore generated protobuf files
    This was done simply by simply exlcuding any pb.ex files

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,8 @@
 # Used by "mix format"
 [
-  inputs: ["*.{ex,exs}", "{apps,config,lib,test}/**/*.{ex,exs}"],
+  inputs:
+    ["*.{ex,exs}", "{apps,config,lib,test}/**/*.{ex,exs}"]
+    |> Enum.flat_map(&Path.wildcard(&1, match_dot: true))
+    |> Enum.reject(&(&1 =~ ~r/^.*.pb.ex/)),
   line_length: 78
 ]
