b164cb7ab8deba1669d587a825206d8fb95de8fe
Raymond E. Pasco <raymond@heliax.dev>
2024-10-15 10:49:28 -0400

    all: require elixir 1.17

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Anoma.MixProject do
     [
       apps_path: "apps",
       version: version(),
-      elixir: "~> 1.15",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
