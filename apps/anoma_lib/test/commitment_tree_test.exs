c9ce30bad0f2b262cdb415f11dd488696597631a
Mariusz Zak <mariuszzak21@gmail.com>
2024-10-17 08:56:59 +0100

    test helpers: macro to generate tests from examples

--- apps/anoma_lib/test/commitment_tree_test.exs
+++ apps/anoma_lib/test/commitment_tree_test.exs
@@ -1,17 +1,12 @@
 defmodule CommitmentTreeTest do
   alias Examples.ECommitmentTree
-  use TestHelper.TestMacro
-  doctest CommitmentTree
 
-  test "sha256 examples" do
-    ECommitmentTree.sha256_32_spec()
-    ECommitmentTree.memory_backed_ct()
-    ECommitmentTree.empty_mnesia_backed_ct()
-    ECommitmentTree.current_tree_mnesia_ct()
-    ECommitmentTree.babylon_mnesia_ct()
-    ECommitmentTree.current_tree_mnesia_ct()
-    ECommitmentTree.lots_of_inserts_ct()
-  end
+  use TestHelper.TestMacro
+
+  use TestHelper.GenerateExampleTests,
+    for: ECommitmentTree
+
+  doctest CommitmentTree
 
   test "cairo poseidon examples" do
     cairo_spec = ECommitmentTree.cairo_poseidon_spec()
--- apps/anoma_node/test/logging_test.exs
+++ apps/anoma_node/test/logging_test.exs
@@ -1,21 +1,6 @@
 defmodule LoggingTest do
   use ExUnit.Case, async: false
 
-  alias Anoma.Node.Examples.ELogging
-
-  test "transaction event examples" do
-    ELogging.check_tx_event()
-    ELogging.check_multiple_tx_events()
-  end
-
-  test "consensus event examples" do
-    ELogging.check_consensus_event()
-    ELogging.check_consensus_event_multiple()
-  end
-
-  test "block event examples" do
-    ELogging.check_block_event()
-    ELogging.check_block_event_multiple()
-    ELogging.check_block_event_leave_one_out()
-  end
+  use TestHelper.GenerateExampleTests,
+    for: Anoma.Node.Examples.ELogging
 end
