anoma/anoma

commit 96448e983760b09cde21f0d535352c91df12e297

anoma_lib: add transparent rm structures

apps/anoma_lib/lib/anoma/transparent_resource/delta.ex
@@ -1,0 +1,17 @@
+defmodule Anoma.TransparentResource.Delta do
+  @moduledoc """
+  delta functions. not a struct don't make it a struct
+  """
+
+  @type t() :: %{binary() => integer()}
+
+  def add(d1 = %{}, d2 = %{}) do
+    Map.merge(d1, d2, fn _k, v1, v2 -> v1 + v2 end)
+    |> Map.reject(fn {_k, v} -> v == 0 end)
+  end
+
+  def sub(d1 = %{}, d2 = %{}) do
+    Map.merge(d1, d2, fn _k, v1, v2 -> v1 - v2 end)
+    |> Map.reject(fn {_k, v} -> v == 0 end)
+  end
+end

apps/anoma_lib/lib/anoma/transparent_resource.ex
@@ -1,0 +1,2 @@
+defmodule Anoma.TransparentResource do
+end

apps/anoma_lib/lib/anoma/transparent_resource/action.ex
@@ -1,0 +1,47 @@
+defmodule Anoma.TransparentResource.Action do
+  use TypedStruct
+
+  alias Anoma.TransparentResource.Delta
+  alias Anoma.TransparentResource.LogicProof
+  alias Anoma.TransparentResource.Resource
+  alias __MODULE__
+
+  typedstruct enforce: true do
+    field(:commitments, MapSet.t(binary()), default: [])
+    field(:nullifiers, MapSet.t(binary()), default: [])
+    field(:proofs, MapSet.t(LogicProof.t()), default: [])
+    field(:app_data, binary(), default: <<>>)
+  end
+
+  def precis(%Action{proofs: proofs}) do
+    for proof <- proofs,
+        reduce: %{commited: MapSet.new(), nullified: MapSet.new()} do
+      %{committed: committed, nullified: nullified} ->
+        case proof.polarity do
+          :committed ->
+            new_committed = MapSet.put(committed, proof.resource)
+            %{committed: new_committed, nullified: nullified}
+
+          :nullified ->
+            new_nullified = MapSet.put(nullified, proof.resource)
+            %{committed: committed, nullified: new_nullified}
+        end
+    end
+  end
+
+  def delta(action = %Action{}) do
+    %{committed: committed, nullified: nullified} = precis(action)
+
+    committed_delta =
+      for resource <- committed, reduce: %{} do
+        acc -> Delta.add(acc, Resource.delta(resource))
+      end
+
+    nullified_delta =
+      for resource <- nullified, reduce: %{} do
+        acc -> Delta.add(acc, Resource.delta(resource))
+      end
+
+    Delta.sub(committed_delta, nullified_delta)
+  end
+end

apps/anoma_lib/lib/anoma/transparent_resource/logic_proof.ex
@@ -1,0 +1,46 @@
+defmodule Anoma.TransparentResource.LogicProof do
+  use TypedStruct
+
+  alias Anoma.TransparentResource.Resource
+  alias __MODULE__
+
+  typedstruct enforce: true do
+    # the resource being proven
+    field(:resource, Resource.t())
+    # public inputs
+    field(:commitments, MapSet.t(Resource.commitment()),
+      default: MapSet.new()
+    )
+
+    field(:nullifiers, MapSet.t(Resource.nullifier()), default: MapSet.new())
+
+    field(
+      :self_tag,
+      {:committed, Resource.commitment()}
+      | {:nullified, Resource.commitment()}
+    )
+
+    field(:other_public, Noun.t(), default: <<>>)
+    # private inputs
+    field(:committed_plaintexts, MapSet.t(Resource.t()),
+      default: MapSet.new()
+    )
+
+    field(:nullified_plaintexts, MapSet.t(Resource.t()),
+      default: MapSet.new()
+    )
+
+    field(:other_private, Noun.t(), default: <<>>)
+  end
+
+  def verify(proof = %LogicProof{}) do
+    args = []
+
+    result = Nock.nock(proof.resource.logic, [9, 2, 10, [6 | args], 0 | 1])
+
+    case result do
+      {:ok, zero} when zero in [0, <<>>, <<0>>, []] -> true
+      _ -> false
+    end
+  end
+end

apps/anoma_lib/lib/anoma/transparent_resource/resource.ex
@@ -1,0 +1,116 @@
+defmodule Anoma.TransparentResource.Resource do
+  @moduledoc """
+  resource struct
+  """
+
+  use TypedStruct
+  alias __MODULE__
+
+  @type committment() :: binary()
+  @type nullifier() :: binary()
+
+  typedstruct enforce: true do
+    # following two are together the kind
+    field(:label, binary(), default: "")
+    field(:logic, Noun.t(), default: [[1 | 0], 0 | 0])
+    # ephemerality flag
+    field(:ephemeral, bool(), default: false)
+    # following are more value-like fields
+    field(:quantity, non_neg_integer(), default: 1)
+    # This field is NOT named "value".
+    # The phrase "The value of resource R" means something.
+    # What it means is:
+    # - The value of the resource's label field, and
+    # - the value of the resource's logic field, and
+    # - the value of the resource's ephemerality flag field, and
+    # - the value of the resource's quantity field, and
+    # - the value of the resource's data field, and
+    # - the value of the resource's nullifier key commitment field, and
+    # - the value of the resource's nonce field, and
+    # - the value of the resource's random seed field,
+    # all together.
+    field(:data, binary(), default: <<>>)
+    # nullifier key. should be a public-key like thing probably
+    field(:nullifier_key, Anoma.Crypto.Sign.public(), default: <<0::256>>)
+    # for uniqueness
+    field(:nonce, <<_::256>>, default: <<0::256>>)
+    # useless field for shielded only.
+    field(:rseed, <<>>, default: <<>>)
+  end
+
+  def to_noun(resource = %Resource{}) do
+    [
+      resource.label,
+      resource.logic,
+      bool_to_noun(resource.ephemeral),
+      resource.quantity,
+      resource.data,
+      resource.nullifier_key,
+      resource.nonce,
+      resource.rseed
+    ]
+  end
+
+  def from_noun([
+        label,
+        logic,
+        ephemeral,
+        quantity,
+        data,
+        nullifier_key,
+        nonce,
+        rseed | terminator
+      ])
+      when terminator in [0, <<>>, <<0>>, []] do
+    {:ok,
+     %Resource{
+       label: label,
+       logic: logic,
+       ephemeral: noun_to_bool(ephemeral),
+       quantity: quantity,
+       data: data,
+       nullifier_key: nullifier_key,
+       nonce: nonce,
+       rseed: rseed
+     }}
+  end
+
+  def from_noun(_) do
+    :error
+  end
+
+  def noun_to_bool(zero) when zero in [0, <<>>, <<0>>, []] do
+    true
+  end
+
+  def noun_to_bool(one) when one in [1, <<1>>] do
+    false
+  end
+
+  def bool_to_noun(true) do
+    0
+  end
+
+  def bool_to_noun(false) do
+    1
+  end
+
+  def kind(%Resource{label: label, logic: logic}) do
+    kind = label <> Noun.atom_integer_to_binary(Nock.Jam.jam(logic))
+    :crypto.hash(:sha256, kind)
+  end
+
+  def delta(resource = %Resource{}) do
+    %{kind(resource) => resource.quantity}
+  end
+
+  def commitment(resource = %Resource{}) do
+    binary_resource = resource |> to_noun() |> Nock.Jam.jam()
+    "CM_" <> binary_resource
+  end
+
+  def nullifier(resource = %Resource{}) do
+    binary_resource = resource |> to_noun() |> Nock.Jam.jam()
+    "NF_" <> binary_resource
+  end
+end

apps/anoma_lib/lib/anoma/transparent_resource/transaction.ex
@@ -1,0 +1,67 @@
+defmodule Anoma.TransparentResource.Transaction do
+  use TypedStruct
+
+  alias Anoma.TransparentResource.Action
+  alias Anoma.TransparentResource.Delta
+  alias __MODULE__
+
+  typedstruct enforce: true do
+    field(:roots, MapSet.t(binary()), default: [])
+    field(:actions, MapSet.t(Action.t()), default: [])
+    field(:delta, Delta.t(), default: %{})
+    # useless field for shielded only.
+    field(:delta_proof, <<>>, default: <<>>)
+  end
+
+  def verify(tx = %Transaction{}) do
+    with true <- verify_tx_roots(tx),
+         true <- verify_tx_action_distinctness(tx),
+         true <- verify_tx_action_compliance(tx),
+         true <- verify_tx_action_delta_sum(tx),
+         true <- verify_tx_has_zero_delta(tx),
+         true <- verify_tx_action_logics(tx) do
+      true
+    else
+      _ -> false
+    end
+  end
+
+  # every consumed resource referenced must exist in a referenced root
+  def verify_tx_roots(_) do
+    true
+  end
+
+  # actions must contain disjoint sets of commitments and nullifiers
+  def verify_tx_action_distinctness(_) do
+    true
+  end
+
+  # actions must be compliant, i.e., contain a proof for each resource
+  def verify_tx_action_compliance(_) do
+    true
+  end
+
+  # the sum of all action deltas we compute here must equal
+  # the transaction delta
+  def verify_tx_action_delta_sum(%Transaction{
+        actions: actions,
+        delta: tx_delta
+      }) do
+    action_delta_sum =
+      for action <- actions, reduce: %{} do
+        acc -> Delta.add(acc, Action.delta(action))
+      end
+
+    action_delta_sum == tx_delta
+  end
+
+  # the tx's delta must be zero
+  def verify_tx_has_zero_delta(%Transaction{delta: delta}) do
+    delta == %{}
+  end
+
+  # all transaction logic proofs must pass
+  def verify_tx_action_logics(_) do
+    true
+  end
+end

hoon/logics.hoon
@@ -2,23 +2,4 @@
 !.
 =>  resource-machine
 |%
-++  balanced-delta
-  ^-  resource-logic
-  |=  [self=resource tx=resource-transaction]
-  ^-  ?
-  =(~ delta.tx)
-++  counter
-  ^-  resource-logic
-  |=  [self=resource tx=resource-transaction]
-  ^-  ?
-  ?.  =(1 (length commitments.tx))
-    |
-  ?.  =(1 (length nullifiers.tx))
-    |
-  ?.  =(1 (length delta.tx))
-    |
-  ?<  ?=(~ delta.tx)
-  ?.  sign.i.delta.tx
-    |
-  &
 --

commit b242f413af9a5a94dd5eccced0d2b764a363c0be

Fix Delta negation Logic
This bug was introduced in:

96448e983760b09cde21f0d535352c91df12e297

However I'm basing it off of:

f2b9ffd4e87ace88759ae0d75d6dbb2456865b2f

because it introduces the negate/1 function.

This bug can be seen in the following way:

iex(mariari@Gensokyo)79> Map.merge(%{}, %{:a => 3}, fn _k, v1, v2 -> v1 - v2 end)
%{a: 3}
iex(mariari@Gensokyo)80> Map.merge(%{:a => 1}, %{:a => 3}, fn _k, v1, v2 -> v1 - v2 end)
%{a: -2}

What was likely meant was:

iex(mariari@Gensokyo)86> Map.merge(%{}, negate(%{:a => 3}), fn _k, v1, v2 -> v1 + v2 end)
%{a: -3}

Which is what the logic now is

apps/anoma_lib/lib/anoma/transparent_resource/delta.ex
@@ -17,8 +17,7 @@ defmodule Anoma.TransparentResource.Delta do
   end
 
   def sub(d1 = %{}, d2 = %{}) do
-    Map.merge(d1, d2, fn _k, v1, v2 -> v1 - v2 end)
-    |> make_sane()
+    add(d1, negate(d2))
   end
 
   def negate(d = %{}) do