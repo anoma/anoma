96e247dac7057cb8b245af00bfcffd4abe9a363b
mariari <mariari@protonmail.com>
2024-10-22 15:58:41 +0800

    Break out logic proof examples to be better composed
    Before we just had the swaps, however the proof logics for just
    commiting and nulifiying a resource should also be had, as they would
    be useful for action proofs that we can compose and test that they
    work correctly

--- apps/anoma_lib/lib/examples/e_transparent/e_logic_proof.ex
+++ apps/anoma_lib/lib/examples/e_transparent/e_logic_proof.ex
@@ -4,13 +4,24 @@ defmodule Examples.ETransparent.ELogicProof do
 
   use TestHelper.TestMacro
 
-  def trivial_true_swap_proof_commitment() do
+  def trivial_true_commitment() do
     res = %LogicProof{
       resource: EResource.trivial_true_resource(),
       self_tag: {:committed, EResource.trivial_true_commitment()},
       commitments: MapSet.new([EResource.trivial_true_commitment()]),
+      committed_plaintexts: MapSet.new([EResource.trivial_true_resource()])
+    }
+
+    assert LogicProof.verify(res)
+
+    res
+  end
+
+  def trivial_true_2_nullifier() do
+    res = %LogicProof{
+      resource: EResource.trivial_true_resource_2(),
+      self_tag: {:nullified, EResource.trivial_true_nullifier_2()},
       nullifiers: MapSet.new([EResource.trivial_true_nullifier_2()]),
-      committed_plaintexts: MapSet.new([EResource.trivial_true_resource()]),
       nullified_plaintexts: MapSet.new([EResource.trivial_true_resource_2()])
     }
 
@@ -19,14 +30,27 @@ defmodule Examples.ETransparent.ELogicProof do
     res
   end
 
-  def trivial_true_swap_proof_nullifier() do
+  def trivial_true_swap_proof_commitment() do
+    nullifier_info = trivial_true_2_nullifier()
+
     res = %LogicProof{
-      resource: EResource.trivial_true_resource_2(),
-      self_tag: {:nullified, EResource.trivial_true_nullifier_2()},
-      commitments: MapSet.new([EResource.trivial_true_commitment()]),
-      nullifiers: MapSet.new([EResource.trivial_true_nullifier_2()]),
-      committed_plaintexts: MapSet.new([EResource.trivial_true_resource()]),
-      nullified_plaintexts: MapSet.new([EResource.trivial_true_resource_2()])
+      trivial_true_commitment()
+      | nullifiers: nullifier_info.nullifiers,
+        nullified_plaintexts: nullifier_info.nullified_plaintexts
+    }
+
+    assert LogicProof.verify(res)
+
+    res
+  end
+
+  def trivial_true_swap_proof_nullifier() do
+    commitment_info = trivial_true_commitment()
+
+    res = %LogicProof{
+      trivial_true_2_nullifier()
+      | commitments: commitment_info.commitments,
+        committed_plaintexts: commitment_info.committed_plaintexts
     }
 
     assert LogicProof.verify(res)
