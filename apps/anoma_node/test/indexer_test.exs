37fc9685b43e5abac45216e057ca9f972ff7f7bf
agureev <artguril@proton.me>
2024-10-23 17:30:55 +0400

    Add Indexer Root Getting Examples

--- apps/anoma_node/test/indexer_test.exs
+++ apps/anoma_node/test/indexer_test.exs
@@ -1,15 +1,4 @@
 defmodule IndexerTest do
   use ExUnit.Case, async: false
-
-  alias Anoma.Node.Examples.EIndexer
-
-  test "indexing examples" do
-    # commented out until multihoming
-    # EIndexer.indexer_reads_height()
-    EIndexer.indexer_reads_nullifier()
-    EIndexer.indexer_reads_nullifiers()
-    EIndexer.indexer_reads_commitments()
-    EIndexer.indexer_does_not_read_revealed()
-    EIndexer.indexer_reads_unrevealed()
-  end
+  use TestHelper.GenerateExampleTests, for: Anoma.Node.Examples.EIndexer
 end
