03e51b6fe8f037994ef8946ec3e6c185c3cbace3
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-10-28 10:45:37 +0100

    disable sasl logs in dev by default

--- config/dev.exs
+++ config/dev.exs
@@ -3,4 +3,16 @@ import Config
 # ----------------------------------------------------------------------------
 # Logger
 
-config :logger,  level: :debug
+# note: to hide the SASL reports in debugging, set `handle_sasl_reports: false`.
+#
+# Example sasl report:
+# 16:13:44.310 [info] Child {:acceptor, #PID<0.3651.0>, 3} of Supervisor #PID<0.3651.0> (:ranch_acceptors_sup) started
+# Pid: #PID<0.3654.0>
+# Start Call: :ranch_acceptor.start_link(#Port<0.123>, :ranch_tcp, :error_logger, #PID<0.3650.0>)
+# Restart: :permanent
+# Shutdown: :brutal_kill
+# Type: :worker
+config :logger,
+  level: :debug,
+  handle_otp_reports: true,
+  handle_sasl_reports: true
