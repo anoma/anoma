16d9d9cde2753ac246d414556e2ef29a9797e45e
Xuyang Song <xuyangsong@hotmail.com>
2024-10-31 22:11:56 +0800

    add the decryption test for cairo RM

--- apps/anoma_lib/lib/examples/e_cairo/e_resource_logic.ex
+++ apps/anoma_lib/lib/examples/e_cairo/e_resource_logic.ex
@@ -1,16 +1,42 @@
 defmodule Examples.ECairo.EResourceLogic do
   use Memoize
 
-  alias Anoma.RM.Shielded.ProofRecord
+  alias Anoma.CairoResource.ProofRecord
   alias Examples.ECairo.EProofRecord
+  alias Anoma.CairoResource.LogicOutput
+  alias Examples.ECairo.EResource
 
   use TestHelper.TestMacro
 
   @spec a_input_resource_logic() :: ProofRecord.t()
   defmemo a_input_resource_logic() do
-    EProofRecord.a_resource_logic(
-      "params/trivial_input_resource_logic_witness.json"
-    )
+    res =
+      EProofRecord.a_resource_logic(
+        "params/trivial_input_resource_logic_witness.json"
+      )
+
+    output = res.public_inputs |> LogicOutput.from_public_input()
+
+    plaintext = LogicOutput.decrypt(output.cipher, <<1::256>>)
+
+    a_resource = EResource.a_fixed_resource()
+
+    expected_text = [
+      a_resource.logic,
+      a_resource.label,
+      a_resource.quantity,
+      a_resource.data,
+      <<0::256>>,
+      a_resource.nonce,
+      a_resource.npk,
+      a_resource.rseed,
+      <<0::256>>,
+      <<0::256>>
+    ]
+
+    assert expected_text == plaintext
+
+    res
   end
 
   @spec a_output_resource_logic() :: ProofRecord.t()
