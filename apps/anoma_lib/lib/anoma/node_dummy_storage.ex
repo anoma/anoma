cb3f7922e6d35451cacdc968b4d658bf2604a1b5
mariari <mariari@protonmail.com>
2024-11-04 13:46:58 +0800

    type dummy_storage

--- apps/anoma_lib/lib/anoma/node_dummy_storage.ex
+++ apps/anoma_lib/lib/anoma/node_dummy_storage.ex
@@ -3,6 +3,7 @@ defmodule Anoma.Node.DummyStorage do
   I am dummy storage during the defactor, to avoid undue stress on shielded rm.
   """
 
+  @spec get(any(), any()) :: {:ok, true}
   def get(_, _) do
     {:ok, true}
   end
