anoma/anoma-archive

check the compliance hash in cairo RM

apps/anoma_lib/lib/anoma/shielded_resource/shielded_transaction.ex
@@ -12,6 +12,7 @@ defmodule Anoma.ShieldedResource.ShieldedTransaction do
   alias Anoma.ShieldedResource.PartialTransaction
   alias Anoma.ShieldedResource.ComplianceOutput
   alias Anoma.Node.DummyStorage, as: Storage
+  alias Anoma.ShieldedResource.ProofRecord
 
   typedstruct enforce: true do
     # TODO: The roots, commitments, and nullifiers can be eliminated. We can
@@ -139,12 +140,7 @@ defmodule Anoma.ShieldedResource.ShieldedTransaction do
         transaction.partial_transactions
         |> Enum.flat_map(fn ptx ->
           ptx.logic_proofs
-          |> Enum.map(fn proof_record ->
-            proof_record.public_inputs
-            |> :binary.bin_to_list()
-            |> Cairo.get_program_hash()
-            |> :binary.list_to_bin()
-          end)
+          |> Enum.map(&ProofRecord.get_cairo_program_hash/1)
         end)
 
       resource_logic_valid =

anoma/anoma

apps/anoma_lib/lib/anoma/cairo_resource/shielded_transaction.ex
@@ -11,6 +11,7 @@ defmodule Anoma.CairoResource.Transaction do
   use TypedStruct
   alias Anoma.CairoResource.PartialTransaction
   alias Anoma.CairoResource.ComplianceOutput
+  alias Anoma.CairoResource.ProofRecord
   alias Anoma.Node.DummyStorage, as: Storage
 
   typedstruct enforce: true do
@@ -139,12 +140,7 @@ defmodule Anoma.CairoResource.Transaction do
         transaction.partial_transactions
         |> Enum.flat_map(fn ptx ->
           ptx.logic_proofs
-          |> Enum.map(fn proof_record ->
-            proof_record.public_inputs
-            |> :binary.bin_to_list()
-            |> Cairo.get_program_hash()
-            |> :binary.list_to_bin()
-          end)
+          |> Enum.map(&ProofRecord.get_cairo_program_hash/1)
         end)
 
       resource_logic_valid =