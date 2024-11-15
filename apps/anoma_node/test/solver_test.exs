ffc7e3cd6b75922c908937cee8b47407f3a0a6f7
agureev <artguril@proton.me>
2024-11-15 15:32:02 +0400

    Make Solver Test use Examples Macro

--- apps/anoma_node/test/solver_test.exs
+++ apps/anoma_node/test/solver_test.exs
@@ -3,12 +3,4 @@ defmodule SolverTest do
 
   use TestHelper.GenerateExampleTests,
     for: Anoma.Node.Examples.ESolver
-
-  alias Anoma.Node.Examples.{ESolver, ENode}
-
-  test "intentpool examples" do
-    ESolver.solvable_transaction_via_intent_pool(
-      ENode.start_node(node_id: "S_test3")
-    )
-  end
 end
