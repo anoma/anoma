656faae89c2a104d589a7a11c7c713244463f9ab
Raymond E. Pasco <raymond@heliax.dev>
2024-11-15 12:48:26 -0500

    special cases so cue("1&") works

--- apps/anoma_lib/lib/nock/nue.ex
+++ apps/anoma_lib/lib/nock/nue.ex
@@ -1,5 +1,6 @@
 defmodule Nue do
   def cue(bytes) do
+    bytes = byte_order_flip(bytes)
     padded_size = bit_size(bytes)
     real_size = real_size(bytes)
 
@@ -15,6 +16,12 @@ defmodule Nue do
       <<rest::size(size - 2)-bitstring, 1::size(1), 0::size(1)>> ->
         {<<>>, rest}
 
+      <<rest::size(size - 4)-bitstring, 12::size(4)>> ->
+        {<<1>>, rest}
+
+      <<rest::size(size - 8)-bitstring, 152::size(8)>> ->
+        {<<4>>, rest}
+
       <<rest::size(size - 2)-bitstring, 0::size(1), 1::size(1)>> ->
         {head, continuation_1} = cue_bits(rest, size - 2)
 
@@ -23,6 +30,12 @@ defmodule Nue do
 
         {[head | tail], continuation_2}
     end
+  end
+
+  defp byte_order_flip(bytes) do
+    bytes
+    |> :binary.decode_unsigned(:little)
+    |> :binary.encode_unsigned()
   end
 
   defp real_size(<<0::1, rest::bitstring>>) do
