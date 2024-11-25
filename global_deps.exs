2d80dda7cf8abaa3690cc7d301989703af102478
Raymond E. Pasco <raymond@heliax.dev>
2024-11-25 11:16:13 -0500

    deps: ex_doc 0.34.2 -> 0.35.1

--- global_deps.exs
+++ global_deps.exs
@@ -25,5 +25,5 @@
   # non-runtime dependencies below
   {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
   {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
-  {:ex_doc, "~> 0.31", only: [:dev], runtime: false}
+  {:ex_doc, "~> 0.35", only: [:dev], runtime: false}
 ]
