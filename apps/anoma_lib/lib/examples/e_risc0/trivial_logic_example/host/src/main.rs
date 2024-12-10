c451e579c8e47c69085ea1df7c0c7ae29120ce65
acentelles <centelles.alberto@gmail.com>
2024-12-10 11:24:30 +0000

    More cleanup

--- apps/anoma_lib/lib/examples/e_risc0/trivial_logic_example/host/src/main.rs
+++ apps/anoma_lib/lib/examples/e_risc0/trivial_logic_example/host/src/main.rs
@@ -11,22 +11,9 @@ fn main() {
         .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
         .init();
 
-    // An executor environment describes the configurations for the zkVM
-    // including program inputs.
-    // An default ExecutorEnv can be created like so:
-    // `let env = ExecutorEnv::builder().build().unwrap();`
-    // However, this `env` does not have any inputs.
-    //
-    // To add guest input to the executor environment, use
-    // ExecutorEnvBuilder::write().
-    // To access this method, you'll need to use ExecutorEnv::builder(), which
-    // creates an ExecutorEnvBuilder. When you're done adding input, call
-    // ExecutorEnvBuilder::build().
-
-    // For example:
-    let input: u32 = 15 * u32::pow(2, 27) + 1;
+    // TODO: Pass resource, resource_nf_key, merkle_path as inputs to .write()
     let env = ExecutorEnv::builder()
-        .write(&input)
+        .write()
         .unwrap()
         .build()
         .unwrap();
@@ -46,7 +33,7 @@ fn main() {
     // TODO: Implement code for retrieving receipt journal here.
 
     // For example:
-    let _output: u32 = receipt.journal.decode().unwrap();
+    // let _output: u32 = receipt.journal.decode().unwrap();
 
     // The receipt was verified at the end of proving, but the below code is an
     // example of how someone else could verify this receipt.
