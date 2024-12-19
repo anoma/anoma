39ebe7c0e2fb6bf73761dc2c5c20a81a309f52ce
Xuyang Song <xuyangsong@hotmail.com>
2024-12-19 23:24:49 +0800

    add zk tag for shieleded tx tests in anoma_node

---
+++ apps/anoma_node/test/shielded_transaction_test.exs
@@ -0,0 +1,8 @@
+defmodule ShieldedTransactionTest do
+  use ExUnit.Case, async: true
+
+  @moduletag :zk
+
+  use TestHelper.GenerateExampleTests,
+    for: Anoma.Node.Examples.EShieldedTransaction
+end
--- apps/anoma_node/test/transaction_test.exs
+++ apps/anoma_node/test/transaction_test.exs
@@ -1,7 +1,4 @@
 defmodule TransactionTest do
   use ExUnit.Case, async: true
   use TestHelper.GenerateExampleTests, for: Anoma.Node.Examples.ETransaction
-
-  use TestHelper.GenerateExampleTests,
-    for: Anoma.Node.Examples.EShieldedTransaction
 end
