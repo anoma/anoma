89a6d5dc6813d848913681c62e1f1ee1c5997542
Terence M. Rokop <rokopt@sharpears.net>
2025-02-05 05:46:08 -0800

    Introduce new files
    Add skeleton files to which to are to be introduced a
    polynomial-functor representation of Nock, and examples of
    its use.

---
+++ apps/anoma_lib/lib/examples/enock_poly.ex
@@ -0,0 +1,8 @@
+defmodule Examples.ENockPoly do
+  use Memoize
+
+  require ExUnit.Assertions
+  import NockPoly
+
+  use TypedStruct
+end
---
+++ apps/anoma_lib/lib/nock_poly.ex
@@ -0,0 +1,12 @@
+defmodule NockPoly do
+  @moduledoc """
+  Representations of polynomial functors in Elixir, and of Nock
+  as a polynomial functor.
+  """
+
+  require Noun
+  require Nock
+  require Logger
+
+  use TypedStruct
+end
