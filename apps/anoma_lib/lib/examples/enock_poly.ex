fd3947aec42cddc1c77d5f6168222a75d24aa082
Terence M. Rokop <rokopt@sharpears.net>
2025-02-05 18:28:56 -0800

    Initial tests for NockPoly

--- apps/anoma_lib/lib/examples/enock_poly.ex
+++ apps/anoma_lib/lib/examples/enock_poly.ex
@@ -2,7 +2,34 @@ defmodule Examples.ENockPoly do
   use Memoize
 
   require ExUnit.Assertions
+  import ExUnit.Assertions
   import NockPoly
+  alias NockPoly.Term, as: Term
 
   use TypedStruct
+
+  @doc """
+  I test `Term.depth/1` and `Term.size/1` (and thus indirectly also
+  `Term.cata/2`) using a variety of terms.
+  """
+  @spec term_tests() :: bool()
+  def term_tests() do
+    t1 = {1, []}
+    assert Term.depth(t1) == 1
+    assert Term.size(t1) == 1
+
+    t2 = {:a, [{:a, []}, {:b, []}]}
+    assert Term.depth(t2) == 2
+    assert Term.size(t2) == 3
+
+    t3 = {:x, [{:y, [{:z, []}]}]}
+    assert Term.depth(t3) == 3
+    assert Term.size(t3) == 3
+
+    t4 = {0, [{1, []}, {2, [{3, []}, {4, []}]}, {5, []}]}
+    assert Term.depth(t4) == 3
+    assert Term.size(t4) == 6
+
+    true
+  end
 end
