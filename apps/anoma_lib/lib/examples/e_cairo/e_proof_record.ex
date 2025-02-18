a25126325fd187ffc352570f92cb194e80709021
Xuyang Song <xuyangsong@hotmail.com>
2025-02-18 14:43:03 +0800

    rename circuit to proving key in cairo rm

--- apps/anoma_lib/lib/examples/e_cairo/e_proof_record.ex
+++ apps/anoma_lib/lib/examples/e_cairo/e_proof_record.ex
@@ -26,13 +26,13 @@ defmodule Examples.ECairo.EProofRecord do
         input_file
       )
 
-    circuit_dir =
+    proving_key_dir =
       Path.join(
         :code.priv_dir(:anoma_lib),
         "params/trivial_resource_logic.json"
       )
 
-    assert {:ok, circuit} = File.read(circuit_dir)
+    assert {:ok, proving_key} = File.read(proving_key_dir)
     assert {:ok, witness} = File.read(witness_dir)
 
     updated_witness =
@@ -45,7 +45,7 @@ defmodule Examples.ECairo.EProofRecord do
 
     assert {:ok, input_resource_logic_proof} =
              ProofRecord.prove(
-               circuit,
+               proving_key,
                updated_witness
              )
 
