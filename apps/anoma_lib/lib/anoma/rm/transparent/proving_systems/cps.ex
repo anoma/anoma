e0fb20be3ebc20ce9638c3046c4e8b84af3038a8
agureev <artguril@proton.me>
2025-02-24 21:03:23 +0400

    Add CPS Nounable Protocol

--- apps/anoma_lib/lib/anoma/rm/transparent/proving_systems/cps.ex
+++ apps/anoma_lib/lib/anoma/rm/transparent/proving_systems/cps.ex
@@ -52,12 +52,14 @@ defmodule Anoma.RM.Transparent.ProvingSystem.CPS.Instance do
     end
   end
 
-  @spec to_noun(t()) :: Noun.t()
-  def to_noun(instance) do
-    [
-      Noun.Nounable.to_noun(instance.consumed),
-      Noun.Nounable.to_noun(instance.created) | instance.unit_delta
-    ]
+  defimpl Noun.Nounable, for: Instance do
+    @impl true
+    def to_noun(instance = %Instance{}) do
+      [
+        Noun.Nounable.to_noun(instance.consumed),
+        Noun.Nounable.to_noun(instance.created) | instance.unit_delta
+      ]
+    end
   end
 end
 
@@ -85,6 +87,7 @@ defmodule Anoma.RM.Transparent.ProvingSystem.CPS do
   alias Anoma.RM.Transparent.Primitive.CommitmentAccumulator
   alias Anoma.RM.Transparent.Resource
   alias Anoma.RM.Transparent.ProvingSystem.CPS.Instance
+  alias __MODULE__
 
   require Logger
   use TypedStruct
@@ -143,7 +146,8 @@ defmodule Anoma.RM.Transparent.ProvingSystem.CPS do
              9,
              2,
              10,
-             [6, 1 | Instance.to_noun(instance)]
+             [6, 1 | Noun.Nounable.to_noun(instance)],
+             0 | 1
            ]) do
       Noun.equal?(res, 0)
     else
@@ -310,13 +314,15 @@ defmodule Anoma.RM.Transparent.ProvingSystem.CPS do
     end
   end
 
-  @spec to_noun(t()) :: Noun.t()
-  def to_noun(t) do
-    [
-      t.proving_key,
-      t.verifying_key,
-      Instance.to_noun(t.instance),
-      <<>> | <<>>
-    ]
+  defimpl Noun.Nounable, for: CPS do
+    @impl true
+    def to_noun(t = %CPS{}) do
+      [
+        t.proving_key,
+        t.verifying_key,
+        Noun.Nounable.to_noun(t.instance),
+        <<>> | <<>>
+      ]
+    end
   end
 end
