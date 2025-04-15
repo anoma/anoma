29f51cf7f2d90e6f280a4a8370bbb2969a4ec56a
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-04-15 16:22:13 +0200

    put transaction type in grpc for mempool

--- apps/anoma_node/lib/node/transport/grpc/endpoint/mempool.ex
+++ apps/anoma_node/lib/node/transport/grpc/endpoint/mempool.ex
@@ -24,7 +24,7 @@ defmodule Anoma.Node.Transport.GRPC.Servers.Mempool do
 
     # create the transaction from the noun
     noun = Jam.cue!(request.transaction.transaction)
-    transaction = {:transparent_resource, noun}
+    transaction = {request.transaction_type, noun}
 
     # submit the transaction to the mempool
     node_id = request.node.id
