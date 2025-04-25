f41b231140e3c7f43fe3d98de65b8f82c061ed6b
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-04-25 14:00:14 +0200

    create proper config setup

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,15 @@
 import Config
 
+# ----------------------------------------------------------------------------
+# Logger
+
 config :logger,
   level: :error,
   handle_otp_reports: false,
   handle_sasl_reports: false
 
 # ----------------------------------------------------------------------------
-# Endpoint
+# Anoma Client Web Endpoint
 
 # Configures the endpoint
 config :anoma_client, Anoma.Client.Web.Endpoint,
@@ -14,27 +17,26 @@ config :anoma_client, Anoma.Client.Web.Endpoint,
   adapter: Bandit.PhoenixAdapter,
   http: [
     ip: {127, 0, 0, 1},
-    port: String.to_integer(System.get_env("CLIENT_HTTP_PORT") || "4000")
+    port: 4000
   ],
   check_origin: false,
   debug_errors: false,
   render_errors: [view: Anoma.Client.Web.ErrorJSON, accepts: ~w(json)]
 
-config :anoma_client,
-  grpc_port: String.to_integer(System.get_env("CLIENT_GRPC_PORT") || "40051")
+# ----------------------------------------------------------------------------
+# Anoma Client
 
-config :anoma_lib, []
+config :anoma_client,
+  grpc_port: 40051
+
+# ----------------------------------------------------------------------------
+# Anoma Node
 
 config :anoma_node,
-  grpc_port: String.to_integer(System.get_env("NODE_GRPC_PORT") || "50051")
+  grpc_port: 50051
 
-config :anoma_protobuf, []
-config :compile_protoc, []
-config :event_broker, []
-
-############################################################
-#                       Mnesia                             #
-############################################################
+# ----------------------------------------------------------------------------
+# Mnesia
 
 # persist_to_disk: should the mnesia data be written to disk?
 #                  default: false
@@ -53,6 +55,4 @@ config :anoma_node, :mnesia,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-if File.exists?("config/#{config_env()}.exs") do
-  import_config "#{config_env()}.exs"
-end
+import_config "#{config_env()}.exs"
--- config/runtime.exs
+++ config/runtime.exs
@@ -1,14 +1,28 @@
 import Config
 
-# ----------------------------------------------------------------------------
-# Endpoint
+# This configuration file is always evaluated at runtime. The other config files
+# are evaluated at compile time.
 
-# Configures the endpoint
-config :anoma_client, Anoma.Client.Web.Endpoint,
-  http: [
-    ip: {127, 0, 0, 1},
-    port: String.to_integer(System.get_env("CLIENT_HTTP_PORT") || "4000")
-  ]
+if Mix.env() == :prod do
+  # ----------------------------------------------------------------------------
+  # Anoma Client Web Endpoint
 
-config :anoma_client,
-  grpc_port: String.to_integer(System.get_env("CLIENT_GRPC_PORT") || "40051")
+  config :anoma_client, Anoma.Client.Web.Endpoint,
+    http: [
+      ip: {127, 0, 0, 1},
+      port: String.to_integer(System.get_env("CLIENT_HTTP_PORT") || "4001")
+    ]
+
+  # ----------------------------------------------------------------------------
+  # Anoma Client
+
+  config :anoma_client,
+    grpc_port:
+      String.to_integer(System.get_env("CLIENT_GRPC_PORT") || "40051")
+
+  # ----------------------------------------------------------------------------
+  # Anoma Node
+
+  config :anoma_node,
+    grpc_port: String.to_integer(System.get_env("NODE_GRPC_PORT") || "50051")
+end
