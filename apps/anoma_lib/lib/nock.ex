aca9b558cd76a014d40d9ce8e608f9d9e87f4afe
mariari <mariari@protonmail.com>
2025-05-12 18:37:41 +0800

    Trash

--- apps/anoma_lib/lib/nock.ex
+++ apps/anoma_lib/lib/nock.ex
@@ -403,7 +403,7 @@ defmodule Nock do
         Logger.debug("nock hint: #{inspect(hint_result)}")
 
       :stdio ->
-        IO.write(environment.stdio, "#{inspect(hint_result, limit: :infinity)}\n")
+        IO.write(environment.stdio, "#{inspect(hint_result)}\n")
 
       _ ->
         # hint_str = Noun.Format.print(hint_result)
