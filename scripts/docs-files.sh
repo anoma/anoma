0ff1249392bda9cc98aa4f04b2f2877b01b2ad80
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-09-10 14:01:06 +0300

    chore: fix file copy in doc script

--- scripts/docs-files.sh
+++ scripts/docs-files.sh
@@ -3,8 +3,8 @@
 # Destination folder where all files will be copied
 destination_folder="./doc/files/"
 
-mkdir -p $destination_folder
+mkdir -p "$destination_folder"
 # Find all folders named "files" and copy their contents to the destination folder
-find ./documentation -type d -name "files" -exec sh -c 'cp -r "$0"/* "$1"' {} "$destination_folder" \;
+find ./documentation -type d -name "files" -exec sh -c 'cp -r "$1"/* "$2"' _ {} "$destination_folder" \;
 
 echo "All files copied and merged into $destination_folder"
